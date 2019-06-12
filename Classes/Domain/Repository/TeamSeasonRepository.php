b7521215efc56ee0298f52a0f44e693b5279a326
Knell <knellcn@medads.uk-erlangen.de>
2019-06-12 16:20:21 +0200

    added debug for clubs

--- Classes/Domain/Repository/TeamSeasonRepository.php
+++ Classes/Domain/Repository/TeamSeasonRepository.php
@@ -2,15 +2,17 @@
 
 
 namespace Balumedien\Clubms\Domain\Repository;
+use \TYPO3\CMS\Extbase\Persistence\Repository;
 
 
-class TeamSeasonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+class TeamSeasonRepository extends Repository {
 
     /**
      * @param \Balumedien\Clubms\Domain\Model\Team $team team item
      */
-    public function findLatestSeasonOfTeam(\Balumedien\Clubms\Domain\Model\Team $team) {
+    public function findLatestSeasonOfTeam($team) {
         // TODO: BUILD QUERY TO FIND LATEST SEASON OF A SPECIFIC TEAM
+        $query->
     }
 
 }
