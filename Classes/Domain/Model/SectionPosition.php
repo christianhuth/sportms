a771cd67f134b0c60669dc7d88705e2f03b2a74b
Christian Knell <christian.knell@balumedien.de>
2019-06-16 15:55:35 +0200

    CLUBMS-184 CLUBMS-185 CLUBMS-186 CLUBMS-187 CLUBMS-188 CLUBMS-189 CLUBMS-190 CLUBMS-191 CLUBMS-192 CLUBMS-193 CLUBMS-194 CLUBMS-195 CLUBMS-196 CLUBMS-197 CLUBMS-198 CLUBMS-199 #done

---
+++ Classes/Domain/Model/SectionPosition.php
@@ -0,0 +1,121 @@
+<?php
+
+
+namespace Balumedien\Clubms\Domain\Model;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * SectionPosition
+ */
+class SectionPosition extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+{
+
+    /**
+     * @var string
+     */
+    protected $label;
+
+    /**
+     * @var string
+     */
+    protected $labelShort;
+
+    /**
+     * @var int
+     */
+    protected $xPosition;
+
+    /**
+     * @var int
+     */
+    protected $yPosition;
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabelShort()
+    {
+        return $this->labelShort;
+    }
+
+    /**
+     * @param string $labelShort
+     */
+    public function setLabelShort($labelShort)
+    {
+        $this->labelShort = $labelShort;
+    }
+
+    /**
+     * @return int
+     */
+    public function getXPosition()
+    {
+        return $this->xPosition;
+    }
+
+    /**
+     * @param int $xPosition
+     */
+    public function setXPosition($xPosition)
+    {
+        $this->xPosition = $xPosition;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYPosition()
+    {
+        return $this->yPosition;
+    }
+
+    /**
+     * @param int $yPosition
+     */
+    public function setYPosition($yPosition)
+    {
+        $this->yPosition = $yPosition;
+    }
+
+}
