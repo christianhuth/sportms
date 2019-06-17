2fc70653cccbed9d11d846d41bf0d93b3e18b133
Christian Knell <christian.knell@balumedien.de>
2019-06-17 19:26:18 +0200

    CLUBMS-397 CLUBMS-398 CLUBMS-399 CLUBMS-408 CLUBMS-409 CLUBMS-410 CLUBMS-411 CLUBMS-412 CLUBMS-413 CLUBMS-414 CLUBMS-415 CLUBMS-416 #done

---
+++ Classes/Domain/Model/TeamSeasonOfficial.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Balumedien\Clubms\Domain\Model;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * TeamSeasonOfficial
+ */
+class TeamSeasonOfficial extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+
+	/**
+	 * @var \Balumedien\Clubms\Domain\Model\Person
+	 */
+	protected $person = '';
+
+	/**
+	 * @var int
+	 */
+	protected $startdate = '';
+
+    /**
+     * @var int
+     */
+    protected $enddate = '';
+
+    /**
+     * @return Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person $person
+     */
+    public function setPerson($person)
+    {
+        $this->person = $person;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStartdate()
+    {
+        return $this->startdate;
+    }
+
+    /**
+     * @param int $startdate
+     */
+    public function setStartdate($startdate)
+    {
+        $this->startdate = $startdate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEnddate()
+    {
+        return $this->enddate;
+    }
+
+    /**
+     * @param int $enddate
+     */
+    public function setEnddate($enddate)
+    {
+        $this->enddate = $enddate;
+    }
+
+}
