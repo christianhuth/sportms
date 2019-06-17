9415e3dd2c025cea6920789b4ac0de6513f6178b
Christian Knell <christian.knell@balumedien.de>
2019-06-17 20:17:58 +0200

    removed unused table definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -731,42 +731,6 @@ CREATE TABLE tx_clubms_domain_model_mail (
 );
 
 #
-# Table structure for table 'tx_clubms_domain_model_mail_type'
-#
-CREATE TABLE tx_clubms_domain_model_mailtype (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	label varchar(255) DEFAULT '' NOT NULL,
-	
-	ordering int(11) DEFAULT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-
-);
-
-#
 # Table structure for table 'tx_clubms_domain_model_official_job'
 #
 CREATE TABLE tx_clubms_domain_model_officialjob (
@@ -1398,42 +1362,6 @@ CREATE TABLE tx_clubms_domain_model_url (
 	club int(11) DEFAULT NULL,
 	club_section int(11) DEFAULT NULL,
 	person int(11) DEFAULT NULL,
-	
-	ordering int(11) DEFAULT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-
-);
-
-#
-# Table structure for table 'tx_clubms_domain_model_urlType'
-#
-CREATE TABLE tx_clubms_domain_model_urltype (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	label varchar(255) DEFAULT '' NOT NULL,
 	
 	ordering int(11) DEFAULT NULL,
 
