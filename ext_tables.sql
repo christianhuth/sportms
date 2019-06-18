059119106288892c1753b4ac79fe909438adaaf7
Christian Knell <christian.knell@balumedien.de>
2019-06-18 17:51:42 +0200

    changed table structure

--- ext_tables.sql
+++ ext_tables.sql
@@ -584,19 +584,20 @@ CREATE TABLE tx_clubms_domain_model_competition (
 
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
-	
-	name varchar(255) DEFAULT '' NOT NULL,
-	short varchar(255) DEFAULT NULL,
-	section int(11) DEFAULT NULL,
-	age_level int(11) DEFAULT NULL,
-	competition_type int(11) DEFAULT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
+    section int(11) DEFAULT '0' NOT NULL,
+    competition_type int(11) DEFAULT '0' NOT NULL,
+    age_level int(11) DEFAULT '0' NOT NULL,
+	name varchar(255) DEFAULT '0' NOT NULL,
+	name_short varchar(255) DEFAULT NULL,
+	competition_seasons int(11) DEFAULT NULL,
 	detail_link tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
 	starttime int(11) unsigned DEFAULT '0' NOT NULL,
 	endtime int(11) unsigned DEFAULT '0' NOT NULL,
 
