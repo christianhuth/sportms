af3b872688e3573e522eef26b84a29ed6c3292bd
Christian Knell <christian.knell@balumedien.de>
2019-06-18 17:59:58 +0200

    added table definition for CompetitionSeason

--- ext_tables.sql
+++ ext_tables.sql
@@ -618,6 +618,44 @@ CREATE TABLE tx_clubms_domain_model_competition (
 );
 
 #
+# Table structure for table 'tx_clubms_domain_model_competitionseason'
+#
+CREATE TABLE tx_clubms_domain_model_competitionseason (
+
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+
+    competition int(11) DEFAULT '0' NOT NULL,
+    season int(11) DEFAULT '0' NOT NULL,
+    max_teams int(11) DEFAULT '0' NOT NULL,
+    gamedays int(11) DEFAULT '0' NOT NULL,
+    games int(11) DEFAULT NULL,
+
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    starttime int(11) unsigned DEFAULT '0' NOT NULL,
+    endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(255) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage int(11) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+
+);
+
+#
 # Table structure for table 'tx_clubms_domain_model_competition_type'
 #
 CREATE TABLE tx_clubms_domain_model_competitiontype (
