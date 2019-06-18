cb683b467d7a3f158b05cb4d640a506ca598625a
Christian Knell <christian.knell@balumedien.de>
2019-06-18 18:04:27 +0200

    added UserFunc for section label

--- Configuration/TCA/UserFunc/UserFunc.php
+++ Configuration/TCA/UserFunc/UserFunc.php
@@ -6,34 +6,40 @@
 		
 		public function addressLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
-			$newAddressLabel = $record['street'] . " " . $record['housenumber'] . " (" . $record['zipcode'] . " " . $record['location'] . ")";
-			$parameters['title'] = $newAddressLabel;
+            $newLabel = $record['street'] . " " . $record['housenumber'] . " (" . $record['zipcode'] . " " . $record['location'] . ")";
+			$parameters['title'] = $newLabel;
 		}
+
+        public function competitionLabel(&$parameters, $parentObject) {
+            $record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
+            $newLabel = $record['name'] . " (" . $record['section'] . " " . $record['age_level'] . ")";
+            $parameters['title'] = $newLabel;
+        }
 		
 		public function phoneLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
-			$newPhoneLabel = $record['areaCode'] . " / " . $record['callingNumber'];
-			$parameters['title'] = $newPhoneLabel;
+            $newLabel = $record['areaCode'] . " / " . $record['callingNumber'];
+			$parameters['title'] = $newLabel;
 		}
 		
 		public function sectionPositionLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
-			$newSectionPositionLabel = $record['label'] . " (" . $record['label_short'] . ")";
-			$parameters['title'] = $newSectionPositionLabel;
+            $newLabel = $record['label'] . " (" . $record['label_short'] . ")";
+			$parameters['title'] = $newLabel;
 		}
 		
 		public function teamSeasonPracticeLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
-			$newTeamSeasonPracticeLabel = $record['day'] . " (" . $record['time_start'] . " - " . $record['time_end'] . " Uhr)";
-			$parameters['title'] = $newTeamSeasonPracticeLabel;
+            $newLabel = $record['day'] . " (" . $record['time_start'] . " - " . $record['time_end'] . " Uhr)";
+			$parameters['title'] = $newLabel;
 		}
 		
 		public function teamSeasonSquadMemberLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 			$person = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_person", $record['person']);
 			$position = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_section_position_group", $record['position']);
-			$newTeamSeasonSquadMemberLabel = $person['lastname'] . ", " . $person['firstname'] . " (" . $position['label'] . ")";
-			$parameters['title'] = $newTeamSeasonSquadMemberLabel;
+            $newLabel = $person['lastname'] . ", " . $person['firstname'] . " (" . $position['label'] . ")";
+			$parameters['title'] = $newLabel;
 		}
 		
 	}
