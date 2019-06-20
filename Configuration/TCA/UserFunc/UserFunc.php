6fc74f2276578e0dc5772c17be7293d527927568
Christian Knell <christian.knell@balumedien.de>
2019-06-20 22:54:25 +0200

    changed to SectionPosition

--- Configuration/TCA/UserFunc/UserFunc.php
+++ Configuration/TCA/UserFunc/UserFunc.php
@@ -40,7 +40,7 @@
 		public function teamSeasonSquadMemberLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 			$person = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_person", $record['person']);
-			$position = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_section_position_group", $record['position']);
+			$position = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_sectionposition", $record['position']);
             $newLabel = $person['lastname'] . ", " . $person['firstname'] . " (" . $position['label'] . ")";
 			$parameters['title'] = $newLabel;
 		}
