7ee87e3bcc70fd786144b6144e048f236ef42616
Christian Knell <christian.knell@uk-erlangen.de>
2019-06-25 15:31:43 +0200

    CLUBMS-1130 #done

--- Configuration/TCA/TeamSeasonOfficialJob.php
+++ Configuration/TCA/TeamSeasonOfficialJob.php
@@ -99,6 +99,9 @@ $GLOBALS['TCA']['tx_clubms_domain_model_teamseasonofficialjob'] = array(
 			'config' => array(
 				'foreign_table' => 'tx_clubms_domain_model_officialjob',
 				'foreign_table_where' => 'ORDER BY label ASC',
+				'items' => array(
+                    array("LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_general.select", ""),
+                ),
 				'maxItems' => 1,
 				'renderType' => 'selectSingle',
 				'size' => 1,
