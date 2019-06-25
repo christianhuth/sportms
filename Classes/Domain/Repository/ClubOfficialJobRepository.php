d272f0752f65d1b23212b9b2dde9af771a48ecc9
Christian Knell <christian.knell@balumedien.de>
2019-06-25 22:27:08 +0200

    CLUBMS-1134 #done

--- Classes/Domain/Repository/ClubOfficialJobRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	namespace Balumedien\Clubms\Domain\Repository;
-	
-	class ClubOfficialJobRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-		
-	}
-	
-?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -292,45 +292,6 @@ CREATE TABLE tx_clubms_domain_model_clubofficial (
 );
 
 #
-# Table structure for table 'tx_clubms_domain_model_clubofficialjob'
-#
-CREATE TABLE tx_clubms_domain_model_clubofficialjob (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	club int(11) DEFAULT NULL,
-
-	club_officials int(11) DEFAULT NULL,
-	official_job int(11) DEFAULT NULL,
-	
-	ordering int(11) DEFAULT NULL,
-	
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-
-);
-
-#
 # Table structure for table 'tx_clubms_domain_model_clubsection'
 #
 CREATE TABLE tx_clubms_domain_model_clubsection (
