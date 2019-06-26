26b067a257d5591603c00c3f2dd0cbe99490f12a
Christian Knell <christian.knell@balumedien.de>
2019-06-26 15:06:33 +0200

    CLUBMS-1231 #done

---
+++ Configuration/TCA/ClubOfficialJob.php
@@ -0,0 +1,99 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$GLOBALS['TCA']['tx_clubms_domain_model_clubofficialjob'] = array(
+	'ctrl' => array(
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'default_sortby' => 'ORDER BY label ASC',
+		'delete' => 'deleted',
+		'dividers2tabs' => TRUE,
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'iconfile' => 'EXT:clubms/Resources/Public/Icons/tx_clubms_domain_model_clubofficialjob.svg',
+		'label' => 'label',
+		'searchFields' => '',
+		'title'	=> 'LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_domain_model_clubofficialjob',
+		'tstamp' => 'tstamp',
+		'versioningWS' => TRUE,
+	),
+	'interface' => array(
+		'showRecordFieldList' => 'label',
+	),
+	'types' => array(
+		'1' => array('showitem' => 'label'),
+	),
+	'palettes' => array(
+		'1' => array('showitem' => ''),
+	),
+	'columns' => array(
+		
+		't3ver_label' => array(
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'max' => 255,
+			)
+		),
+		'hidden' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+			),
+		),
+		'starttime' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
+			'config' => array(
+				'behaviour' => array(
+					'allowLanguageSynchronization' => TRUE,
+				),
+				'type' => 'input',
+				'size' => 13,
+				'eval' => 'datetime',
+				'checkbox' => 0,
+				'default' => 0,
+				'range' => array(
+					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
+				),
+				'renderType' => 'inputDateTime',
+			),
+		),
+		'endtime' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
+			'config' => array(
+				'behaviour' => array(
+					'allowLanguageSynchronization' => TRUE,
+				),
+				'type' => 'input',
+				'size' => 13,
+				'eval' => 'datetime',
+				'checkbox' => 0,
+				'default' => 0,
+				'range' => array(
+					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
+				),
+				'renderType' => 'inputDateTime',
+			),
+		),
+		
+		'label' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_domain_model_clubofficialjob.label',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'eval' => 'trim, required'
+			),
+		),
+		
+	),
+);
