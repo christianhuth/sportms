e1e890d7ed9dd746ef5059d2687771224f67744f
Christian Knell <christian.knell@balumedien.de>
2019-06-30 22:41:47 +0200

    added even more debugging

--- Configuration/TCA/UserFunc/FlexFormUserFunc.php
+++ Configuration/TCA/UserFunc/FlexFormUserFunc.php
@@ -15,6 +15,10 @@
 			$sectionRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\SectionRepository');
 			\TYPO3\CMS\Core\Utility\DebugUtility::debug($sectionRepository, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 
+			$queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
+			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($sectionRepository->findAll())->getSQL());
+			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($sectionRepository->findAll())->getParameters());
+
 			// Get data from repository
 			$myData = $sectionRepository->findAll();
 			\TYPO3\CMS\Core\Utility\DebugUtility::debug($myData, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
