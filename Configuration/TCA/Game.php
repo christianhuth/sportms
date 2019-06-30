b275da25454677ac998b1a6ea5c604a923bbe2a3
Christian Knell <christian.knell@balumedien.de>
2019-06-30 23:12:46 +0200

    changed database query

--- Configuration/TCA/Game.php
+++ Configuration/TCA/Game.php
@@ -153,8 +153,13 @@ $GLOBALS['TCA']['tx_clubms_domain_model_game'] = array(
             'label' => 'LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_domain_model_game.team_home',
             'config' => array(
                 'eval' => 'required',
-                'foreign_table' => 'tx_clubms_domain_model_teamseason',
-                'foreign_table_where' => ' AND tx_clubms_domain_model_teamseason.season = ###REC_FIELD_season### ORDER BY team ASC',
+                'foreign_table' => 'tx_clubms_domain_model_team',
+                #'foreign_table_where' => ' AND tx_clubms_domain_model_teamseason.season = ###REC_FIELD_season### ORDER BY team ASC',
+	            'foreign_table_where' => '  AND tx_clubms_domain_model_team.uid IN 
+	                                            (SELECT uid_foreign FROM tx_clubms_competitionseason_teamseason_mm WHERE uid_local = 
+	                                                (SELECT uid FROM tx_clubms_competitionseason WHERE competition = ###REC_FIELD_competition### AND season = ###REC_FIELD_season###)
+	                                            ) 
+	                                        ORDER BY tx_clubms_domain_model_team.name ASC',
                 'items' => Array (
                     array("LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_general.select", ""),
                 ),
