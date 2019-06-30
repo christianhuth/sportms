71f6a906eb103499aed067cb6cca777839d4e7c8
Christian Knell <christian.knell@balumedien.de>
2019-06-30 22:57:48 +0200

    changed order

--- Configuration/TCA/Team.php
+++ Configuration/TCA/Team.php
@@ -111,7 +111,7 @@ $GLOBALS['TCA']['tx_clubms_domain_model_team'] = array(
             'config' => array(
                 'eval' => 'required',
                 'foreign_table' => 'tx_clubms_domain_model_section',
-                'foreign_table_where' => ' AND tx_clubms_domain_model_section.uid IN (SELECT section FROM tx_clubms_domain_model_clubsection WHERE club = ###REC_FIELD_club###) ORDER BY section ASC',
+                'foreign_table_where' => ' AND tx_clubms_domain_model_section.uid IN (SELECT section FROM tx_clubms_domain_model_clubsection WHERE club = ###REC_FIELD_club###) ORDER BY label ASC',
                 'items' => Array (
                     array("LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_general.select", ""),
                 ),
