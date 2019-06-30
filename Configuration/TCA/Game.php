2f6ab1b86e064a5e89ff6ae4bdb42e6e8803f58a
Christian Knell <christian.knell@balumedien.de>
2019-06-30 23:14:51 +0200

    changed database query for team_home and team_guest

--- Configuration/TCA/Game.php
+++ Configuration/TCA/Game.php
@@ -154,7 +154,6 @@ $GLOBALS['TCA']['tx_clubms_domain_model_game'] = array(
             'config' => array(
                 'eval' => 'required',
                 'foreign_table' => 'tx_clubms_domain_model_team',
-                #'foreign_table_where' => ' AND tx_clubms_domain_model_teamseason.season = ###REC_FIELD_season### ORDER BY team ASC',
 	            'foreign_table_where' => '  AND tx_clubms_domain_model_team.uid IN 
 	                                            (SELECT uid_foreign FROM tx_clubms_competitionseason_teamseason_mm WHERE uid_local = 
 	                                                (SELECT uid FROM tx_clubms_domain_model_competitionseason WHERE competition = ###REC_FIELD_competition### AND season = ###REC_FIELD_season###)
@@ -178,7 +177,11 @@ $GLOBALS['TCA']['tx_clubms_domain_model_game'] = array(
             'config' => array(
                 'eval' => 'required',
                 'foreign_table' => 'tx_clubms_domain_model_teamseason',
-                'foreign_table_where' => ' AND tx_clubms_domain_model_teamseason.season = ###REC_FIELD_season### ORDER BY team ASC',
+	            'foreign_table_where' => '  AND tx_clubms_domain_model_team.uid IN 
+	                                            (SELECT uid_foreign FROM tx_clubms_competitionseason_teamseason_mm WHERE uid_local = 
+	                                                (SELECT uid FROM tx_clubms_domain_model_competitionseason WHERE competition = ###REC_FIELD_competition### AND season = ###REC_FIELD_season###)
+	                                            ) 
+	                                        ORDER BY tx_clubms_domain_model_team.name ASC',
                 'items' => Array (
                     array("LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_general.select", ""),
                 ),
