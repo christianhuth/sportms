7c87d54a6fc935f39cb9fbfec06c626d30cafe35
Christian Knell <christian.knell@balumedien.de>
2019-06-30 22:46:25 +0200

    changed debugging

--- Configuration/TCA/UserFunc/FlexFormUserFunc.php
+++ Configuration/TCA/UserFunc/FlexFormUserFunc.php
@@ -13,11 +13,12 @@
 			// Get repository
 			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 			$sectionRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\SectionRepository');
-			\TYPO3\CMS\Core\Utility\DebugUtility::debug($sectionRepository, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+
+
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($fConfig, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 
 			// Get data from repository
 			$myData = $sectionRepository->findAll();
-			\TYPO3\CMS\Core\Utility\DebugUtility::debug($myData, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 			foreach ($myData as $data) {
 				// push it into the config array
 				array_push($fConfig['section'], array(
