b5185c41508fe5bbd910852ba9a09e63a5948d9a
Christian Knell <christian.knell@balumedien.de>
2019-07-02 11:55:56 +0200

    tried a database query with parent access

--- Configuration/TCA/Game.php
+++ Configuration/TCA/Game.php
@@ -252,9 +252,9 @@ $GLOBALS['TCA']['tx_clubms_domain_model_game'] = array(
                     'columns' => array(
                         'section_position' => array(
                             'config' => array(
-                                'foreign_table_where' => '  AND tx_clubms_domain_model_sectionposition.section_position_group IN
-                                                    (SELECT uid FROM tx_clubms_domain_model_sectionpositiongroup WHERE section = ###REC_FIELD_section###)
-                                                ORDER BY tx_clubms_domain_model_sectionposition.sorting ASC',
+                                'foreign_table_where' => '  AND tx_clubms_domain_model_sectionposition.sectioosition_group IN
+                                                            (SELECT uid FROM tx_clubms_domain_model_sectionpositiongroup WHERE section = ###REC_FIELD_section###)
+                                                            ORDER BY tx_clubms_domain_model_sectionposition.sorting ASC',
                             ),
                         ),
                     ),
