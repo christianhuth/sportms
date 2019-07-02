95e1fcb5cefdfab33df32df6b4a87d4e0912c260
Christian Knell <knellcn@medads.uk-erlangen.de>
2019-07-02 10:24:38 +0200

    changed database query for section

--- Configuration/TCA/Team.php
+++ Configuration/TCA/Team.php
@@ -111,7 +111,7 @@ $GLOBALS['TCA']['tx_clubms_domain_model_team'] = array(
             'config' => array(
                 'eval' => 'required',
                 'foreign_table' => 'tx_clubms_domain_model_clubsection',
-                'foreign_table_where' => ' AND tx_clubms_domain_model_clubsection WHERE club = ###REC_FIELD_club### ORDER BY section ASC',
+                'foreign_table_where' => ' AND tx_clubms_domain_model_clubsection.club = ###REC_FIELD_club### ORDER BY section ASC',
                 'items' => Array (
                     array("LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_general.select", ""),
                 ),
