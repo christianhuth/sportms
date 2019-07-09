8dac81d5b2f671574c87afbf6ba606fad57431e6
Christian Knell <christian.knell@uk-erlangen.de>
2019-07-09 15:45:02 +0200

    CLUBMS-1288 CLUBMS-1291 CLUBMS-1292 CLUBMS-1295 CLUBMS-1296 CLUBMS-1297 CLUBMS-1298 CLUBMS-1299 CLUBMS-1300 CLUBMS-1301 CLUBMS-1302 CLUBMS-1303 CLUBMS-1304 CLUBMS-1305 CLUBMS-1306 CLUBMS-1307 #done

---
+++ Classes/Domain/Model/GameReport.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace Balumedien\Clubms\Domain\Model;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * GameReport
+ */
+class GameReport extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+{
+
+    /**
+     * @var \Balumedien\Clubms\Domain\Model\Game
+     * @lazy
+     */
+    protected $game;
+
+    /**
+     * @var string
+     */
+    protected $headline;
+
+    /**
+     * @var string
+     */
+    protected $text;
+
+    /**
+     * @var string
+     */
+    protected $author;
+
+    /**
+     * @return Game
+     */
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    /**
+     * @param Game $game
+     */
+    public function setGame($game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHeadline()
+    {
+        return $this->headline;
+    }
+
+    /**
+     * @param string $headline
+     */
+    public function setHeadline($headline)
+    {
+        $this->headline = $headline;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+}
