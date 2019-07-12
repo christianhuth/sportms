938e91cd8c17c633d63a25d1d021236d991e9142
Christian Knell <christian.knell@balumedien.de>
2019-07-12 19:08:22 +0200

    testing

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -10,14 +10,13 @@
 		 */
 		public function team_season_squad_member_GameLineup(&$config) {
 
-			// Get repository
-			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
-			$teamSeasonSquadMemberRepository = $objectManager->get('Balumedien\\Clubms\\Domain\\Repository\\TeamSeasonSquadMemberRepository');
-
 			$databaseTable = "tx_clubms_domain_model_teamseasonsquadmember";
-
 			$queryBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)->getQueryBuilderForTable($databaseTable);
-			$data = $queryBuilder->select('*')->from($databaseTable)->execute()->fetchAll();
+			$data = $queryBuilder
+					->select('*')
+					->from($databaseTable)
+					->execute()
+					->fetchAll();
 
 			foreach ($data as $ydata) {
 				// push it into the config array
