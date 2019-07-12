a60f3d7b55b51e48cdc484586e6a13c9aab89926
Christian Knell <christian.knell@balumedien.de>
2019-07-12 19:05:28 +0200

    testing

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -14,33 +14,15 @@
 			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 			$teamSeasonSquadMemberRepository = $objectManager->get('Balumedien\\Clubms\\Domain\\Repository\\TeamSeasonSquadMemberRepository');
 
-			array_push($config['items'], ['Fussball','1']);
-
 			$databaseTable = "tx_clubms_domain_model_teamseasonsquadmember";
 
 			$queryBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)->getQueryBuilderForTable($databaseTable);
 			$data = $queryBuilder->select('*')->from($databaseTable)->execute()->fetchAll();
 
-			array_push($config['items'], ['Fussball','1']);
-
-			#array_push($config['items'], ['byTeamSeasonUid: ' . sizeof($byTeamSeasonUid), sizeof($byTeamSeasonUid)]);
-
-			if(is_null($data)) {
-				array_push($config['items'], ['null','2']);
+			foreach ($data as $ydata) {
+				// push it into the config array
+				array_push($config['items'], [$ydata['person'], $ydata['uid']]);
 			}
-
-			if($data) {
-				array_push($config['items'], [count($data), '2']);
-				foreach ($data as $ydata) {
-					// push it into the config array
-					array_push($config['items'], ['Test', '5']);
-				}
-			} else {
-				array_push($config['items'], ['empty','2']);
-			}
-
-			array_push($config['items'], ['Fussball','3']);
-
 
 		}
 
