8332bc1d0cf762710684867c47a28842f038c2f6
Christian Knell <christian.knell@balumedien.de>
2019-07-12 15:16:30 +0200

    testing

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -10,8 +10,6 @@
 		 */
 		public function team_season_squad_member_GameLineup(&$config) {
 
-			$itemList = [];
-
 			// Get repository
 			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 			$teamSeasonSquadMemberRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\TeamSeasonSquadMemberRepository');
@@ -22,19 +20,13 @@
 			#var_dump("TEST2");
 
 			// Get data from repository
-			#$myData = $teamSeasonSquadMemberRepository->findAll();
-			#foreach ($myData as $data) {
-		#		// push it into the config array
-	#			array_push($config['items'], array(
-	#				$data['label'],
-	#				$data['uid']
-	#			));
-	#		}
+			$myData = $teamSeasonSquadMemberRepository->findAll();
+			foreach ($myData as $data) {
+				// push it into the config array
+				array_push($config['items'], [$data['label'], $data['uid']]);
+			}
 
 			array_push($config['items'], ['Fussball','1']);
-
-			#$config['items'] = $itemList;
-			#return $config;
 
 
 		}
