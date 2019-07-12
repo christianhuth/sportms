eb4760f1c9751bb8a3ec384e71fc0f5128cf47d1
Christian Knell <christian.knell@balumedien.de>
2019-07-12 22:19:33 +0200

    changed default value of own_goal

--- Configuration/TCA/GameGoal.php
+++ Configuration/TCA/GameGoal.php
@@ -187,7 +187,7 @@ $GLOBALS['TCA']['tx_clubms_domain_model_gamegoal'] = array(
 			'exclude' => 1,
 			'label' => 'LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_domain_model_gamegoal.own_goal',
 			'config' => array(
-				'default' => '1',
+				'default' => '0',
 				'type' => 'check',
 			),
 		),
