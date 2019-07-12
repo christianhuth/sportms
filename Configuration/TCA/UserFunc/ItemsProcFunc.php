172c64f128b95328e5d91a5be506406b02e7f4e1
Christian Knell <christian.knell@balumedien.de>
2019-07-12 19:02:15 +0200

    testing

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -19,7 +19,11 @@
 			$databaseTable = "tx_clubms_domain_model_teamseasonsquadmember";
 
 			$queryBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)->getQueryBuilderForTable($databaseTable);
-			$data = $queryBuilder->select('uid, person')->from($databaseTable);
+			$data = $queryBuilder
+					->select('uid, person')
+					->from($databaseTable)
+					->execute()
+					->fetchAll();
 
 			array_push($config['items'], ['Fussball','1']);
 
