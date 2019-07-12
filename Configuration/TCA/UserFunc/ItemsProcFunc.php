8a6a2dbfdc1225854622fdb465ba6237c907256c
Christian Knell <christian.knell@balumedien.de>
2019-07-12 19:19:00 +0200

    testing

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -12,21 +12,23 @@
 
 			$databaseTable = "tx_clubms_domain_model_teamseasonsquadmember";
 			$queryBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)->getQueryBuilderForTable($databaseTable);
-			$data = $queryBuilder
-					->select('teamseasonsquadmember.uid', 'firstname', 'lastname')
-					->from($databaseTable, "teamseasonsquadmember")
-					->innerJoin(
-						$databaseTable,
-						"tx_clubms_domain_model_person",
-						"person",
-						$queryBuilder->expr()->eq('person.uid', $queryBuilder->quoteIdentifier($databaseTable . '.person'))
-					)
-					->execute()
-					->fetchAll();
+			$queryBuilder
+				->select('teamseasonsquadmember.uid', 'firstname', 'lastname')
+				->from($databaseTable, "teamseasonsquadmember")
+				->innerJoin(
+					$databaseTable,
+					"tx_clubms_domain_model_person",
+					"person",
+					$queryBuilder->expr()->eq('person.uid', $queryBuilder->quoteIdentifier($databaseTable . '.person'))
+				);
 
-			foreach ($data as $ydata) {
+			array_push($config['items'], [$queryBuilder->getSQL(), '0']);
+
+			$result = $queryBuilder->execute()->fetchAll();
+
+			foreach ($result as $row) {
 				// push it into the config array
-				array_push($config['items'], [$ydata['lastname'] . ", " . $ydata['firstname'], $ydata['uid']]);
+				array_push($config['items'], [$row['lastname'] . ", " . $row['firstname'], $row['uid']]);
 			}
 
 		}
