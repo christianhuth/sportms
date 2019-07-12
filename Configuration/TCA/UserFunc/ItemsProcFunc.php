b65189ebed4411374df0194275ff611c7b49479c
Christian Knell <christian.knell@balumedien.de>
2019-07-12 15:00:51 +0200

    added debug output

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -12,12 +12,13 @@
 
 			// Get repository
 			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
-			$sectionRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\SectionRepository');
+			$teamSeasonSquadMemberRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\TeamSeasonSquadMemberRepository');
 
 			\TYPO3\CMS\Core\Utility\DebugUtility::debug($fConfig, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($teamSeasonSquadMemberRepository, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 
 			// Get data from repository
-			$myData = $sectionRepository->findAll();
+			$myData = $teamSeasonSquadMemberRepository->findAll();
 			foreach ($myData as $data) {
 				// push it into the config array
 				array_push($fConfig['items'], array(
