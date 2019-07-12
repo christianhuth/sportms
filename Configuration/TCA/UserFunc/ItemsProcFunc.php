9b99a6d5c870c07e80dad846e3ab80683d638613
Christian Knell <christian.knell@balumedien.de>
2019-07-12 15:08:01 +0200

    changed parameter

--- Configuration/TCA/UserFunc/ItemsProcFunc.php
+++ Configuration/TCA/UserFunc/ItemsProcFunc.php
@@ -5,10 +5,10 @@
 	class ItemsProcFunc {
 
 		/**
-		 * @param array $fConfig
+		 * @param array $config
 		 * @return void
 		 */
-		public function team_season_squad_member_GameLineup(&$fConfig) {
+		public function team_season_squad_member_GameLineup(&$config) {
 
 			var_dump("TEST");
 
@@ -16,7 +16,7 @@
 			$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 			$teamSeasonSquadMemberRepository = $objectManager->get('Balumedien\Clubms\Domain\Repository\TeamSeasonSquadMemberRepository');
 
-			\TYPO3\CMS\Core\Utility\DebugUtility::debug($fConfig, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($config, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 			\TYPO3\CMS\Core\Utility\DebugUtility::debug($teamSeasonSquadMemberRepository, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 
 			var_dump("TEST2");
@@ -25,13 +25,13 @@
 			$myData = $teamSeasonSquadMemberRepository->findAll();
 			foreach ($myData as $data) {
 				// push it into the config array
-				array_push($fConfig['items'], array(
+				array_push($config['items'], array(
 					$data['label'],
 					$data['uid']
 				));
 			}
 
-			array_push($fConfig['items'], array(
+			array_push($config['items'], array(
 				'Fussball',
 				'1'
 			));
