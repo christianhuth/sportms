f7f6113e05b643e0bd482f6dca3e6e26e3945785
Christian Knell <christian.knell@balumedien.de>
2019-07-12 22:09:11 +0200

    added database definition for GameGoal

--- ext_tables.sql
+++ ext_tables.sql
@@ -703,6 +703,52 @@ CREATE TABLE tx_clubms_domain_model_gamechange (
 );
 
 #
+# Table structure for table 'tx_clubms_domain_model_gamechange'
+#
+CREATE TABLE tx_clubms_domain_model_gamegoal (
+
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+
+    game int(11) DEFAULT '0' NOT NULL,
+
+    goal_home int(11) DEFAULT '0' NOT NULL,
+    goal_guest int(11) DEFAULT '0' NOT NULL,
+    period int(11) DEFAULT NULL,
+    minute int(11) DEFAULT '0' NOT NULL,
+    minute_additional int(11) DEFAULT '0' NOT NULL,
+    scorer int(11) DEFAULT '0' NOT NULL,
+    assist int(11) DEFAULT '0' NOT NULL,
+    own_goal tinyint(4) DEFAULT '0' NOT NULL,
+    goal_type int(11) DEFAULT NULL,
+
+    sorting int(11) DEFAULT '0' NOT NULL,
+
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    starttime int(11) unsigned DEFAULT '0' NOT NULL,
+    endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(255) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage int(11) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+
+);
+
+#
 # Table structure for table 'tx_clubms_domain_model_gamelineup'
 #
 CREATE TABLE tx_clubms_domain_model_gamelineup (
