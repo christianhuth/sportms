4e8b5e2196c840ff6df18c7b76dccd25d6287cd9
Christian Knell <christian.knell@balumedien.de>
2019-07-12 13:00:46 +0200

    added table definition for GameChange

--- ext_tables.sql
+++ ext_tables.sql
@@ -655,6 +655,49 @@ CREATE TABLE tx_clubms_domain_model_game (
 );
 
 #
+# Table structure for table 'tx_clubms_domain_model_gamechange'
+#
+CREATE TABLE tx_clubms_domain_model_gamechange (
+
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+
+    game int(11) DEFAULT '0' NOT NULL,
+
+    period int(11) DEFAULT NULL,
+    minute int(11) DEFAULT '0' NOT NULL,
+    minute_additional int(11) DEFAULT '0' NOT NULL,
+    person_in int(11) DEFAULT '0' NOT NULL,
+    person_out int(11) DEFAULT '0' NOT NULL,
+    reason int(11) DEFAULT NULL,
+
+    sorting int(11) DEFAULT '0' NOT NULL,
+
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    starttime int(11) unsigned DEFAULT '0' NOT NULL,
+    endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(255) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage int(11) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+
+);
+
+#
 # Table structure for table 'tx_clubms_domain_model_gamelineup'
 #
 CREATE TABLE tx_clubms_domain_model_gamelineup (
