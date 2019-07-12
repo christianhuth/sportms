196fdb2828e07af2d932c921b5ed1d7de0157e0e
Christian Knell <christian.knell@balumedien.de>
2019-07-12 17:19:35 +0200

    testing

--- Classes/Domain/Repository/TeamSeasonSquadMemberRepository.php
+++ Classes/Domain/Repository/TeamSeasonSquadMemberRepository.php
@@ -11,10 +11,7 @@ class TeamSeasonSquadMemberRepository extends \TYPO3\CMS\Extbase\Persistence\Rep
 	 */
 	public function findByTeamSeasonUid($teamSeasonUid) {
 		$query = $this->createQuery();
-		$query->matching($query->equals('team_season', $teamSeasonUid));
-		$queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
-		\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
-		\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters());
+		$query->matching($query->equals('team_season', 1));
 		return $query->execute();
 	}
 
