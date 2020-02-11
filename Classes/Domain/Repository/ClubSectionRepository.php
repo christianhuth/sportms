c5366f3ffc52822e82d4eb20d61bca0787b2166f
Christian Knell <christian.knell@balumedien.de>
2020-02-12 00:12:00 +0100

    removed old function

--- Classes/Domain/Repository/ClubSectionRepository.php
+++ Classes/Domain/Repository/ClubSectionRepository.php
@@ -3,10 +3,7 @@
 	namespace Balumedien\Clubms\Domain\Repository;
 	
 	class ClubSectionRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-		
-		public function findAllByClub(\Balumedien\Clubms\Domain\Model\Club $club) {
-			
-		}
+	
 	}
 	
 ?>
