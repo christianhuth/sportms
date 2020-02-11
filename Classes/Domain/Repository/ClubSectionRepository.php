5828b2a2bb187dd05b5163fbdba8ea05d81d25b5
Christian Knell <christian.knell@balumedien.de>
2020-02-12 00:33:12 +0100

    debug query

--- Classes/Domain/Repository/ClubSectionRepository.php
+++ Classes/Domain/Repository/ClubSectionRepository.php
@@ -9,8 +9,16 @@
 		];
 		
 		public function findByClubUid($clubUid) {
+			
+			
+			
 			$query = $this->createQuery();
 			$query->matching($query->equals('club', $clubUid));
+			
+			# Folgender Schnippel debugged eine Query, die in der Variable $query gespeichert ist
+			$queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
+			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
+			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters());
 			return $query->execute();
 		}
 		
