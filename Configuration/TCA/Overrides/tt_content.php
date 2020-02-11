96bfe0696949e24aaa75ac4ce945eebe6793e575
Christian Knell <christian.knell@balumedien.de>
2020-02-11 22:44:20 +0100

    moved even more stuff from ext_tables.php to Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,2 +1,22 @@
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+<?php
+	defined('TYPO3_MODE') or die();
+	
+	/***************
+	 * Register Plugin in the Backend Plugin List
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+		$_EXTKEY,					                        // Extension-Key
+		'clubms',					                        // Plugin-Name
+		'Club Management System',	                        // Plugin-Label
+		'EXT:clubms/Resources/Public/Icons/Extension.png'   // Extension Icon in Plugin Selection
+	);
+	
+	$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+	$pluginName = strtolower('clubms');
+	$pluginSignature = $extensionName.'_'.$pluginName;
+	
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+		$pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_clubms.xml'
+	);
--- ext_tables.php
+++ ext_tables.php
@@ -7,20 +7,4 @@
 	// Add Static Template File
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Club Management System');
 	
-	// Register Plugin in the Backend Plugin List
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-		$_EXTKEY,					                        // Extension-Key
-		'clubms',					                        // Plugin-Name
-		'Club Management System',	                        // Plugin-Label
-        'EXT:clubms/Resources/Public/Icons/Extension.png'   // Extension Icon in Plugin Selection
-	);
-	
-	$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
-	$pluginName = strtolower('clubms');
-	$pluginSignature = $extensionName.'_'.$pluginName;
-	
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-		$pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_clubms.xml'
-	);
-	
 ?>
