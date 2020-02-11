ebce1ef0251b15120ca1cc0b170b3c683d20f24e
Christian Knell <christian.knell@balumedien.de>
2020-02-12 00:14:08 +0100

    adding default ordering

--- Classes/Domain/Repository/ClubSectionRepository.php
+++ Classes/Domain/Repository/ClubSectionRepository.php
@@ -3,7 +3,9 @@
 	namespace Balumedien\Clubms\Domain\Repository;
 	
 	class ClubSectionRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-	
+		protected $defaultOrderings = [
+			'section.label' => QueryInterface::ORDER_ASCENDING,
+		];
 	}
 	
 ?>
