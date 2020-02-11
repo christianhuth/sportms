20fca5eb4d36337d7e6a1c6729a1a9ddfa9b34b2
Christian Knell <christian.knell@balumedien.de>
2020-02-11 22:40:27 +0100

    moved TCA Overrides to single files

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,2 @@
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
--- ext_tables.php
+++ ext_tables.php
@@ -19,8 +19,6 @@
 	$pluginName = strtolower('clubms');
 	$pluginSignature = $extensionName.'_'.$pluginName;
 	
-	$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-	$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		$pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_clubms.xml'
 	);
