62c2915c81c63e7e260212f08db340ed5a5590c5
Christian Knell <christian.knell@balumedien.de>
2020-02-24 18:54:24 +0100

    wip

--- Configuration/TCA/PersonProfile.php
+++ Configuration/TCA/PersonProfile.php
@@ -126,7 +126,12 @@ $GLOBALS['TCA']['tx_clubms_domain_model_personprofile'] = array(
 			'onChange' => 'reload',
 		),
 		'section_position_group' => array(
-			'displayCond' => 'FIELD:section:>:0',
+			'displayCond' => array(
+				'AND' => array(
+					'FIELD:profile_type:=:player',
+					'FIELD:section:>:0',
+				),
+			),
 			'exclude' => 1,
 			'label' => 'LLL:EXT:clubms/Resources/Private/Language/locallang_tca.xlf:tx_clubms_domain_model_personprofile.section_position_group',
 			'config' => array(
