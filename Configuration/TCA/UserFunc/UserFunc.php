ec9505db4f888e01ce01ddf3eb4ee3b924509cb1
Christian Knell <christian.knell@balumedien.de>
2020-02-24 17:54:20 +0100

    wip

--- Configuration/TCA/UserFunc/UserFunc.php
+++ Configuration/TCA/UserFunc/UserFunc.php
@@ -172,7 +172,13 @@
 		public function personProfileLabel(&$parameters, $parentObject) {
 			$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
 			$section = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_section", $record['section']);
-			$newLabel = $section['label'];
+			$profiletype = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_clubms_domain_model_section", $record['profiletype']);
+			switch($profiletype) {
+				case "official": $profiletype = "FunktionärIn"; break;
+				case "player": $profiletype = "SpielerIn"; break;
+				case "referee": $profiletype = "SchiedsrichterIn"; break;
+			}
+			$newLabel = $section['label'] . " (" . $profiletype . ")";
 			$parameters['title'] = $newLabel;
 		}
 		
