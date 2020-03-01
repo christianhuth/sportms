dde700d40d68fe5fa8a89178fbb39e73eeaba9c8
Christian Knell <christian.knell@balumedien.de>
2020-03-01 22:23:08 +0100

    added UrlRepository

--- Classes/Domain/Repository/TeamSeasonRepository.php
+++ Classes/Domain/Repository/TeamSeasonRepository.php
@@ -1,17 +1,14 @@
 <?php
-
-
-namespace Balumedien\Clubms\Domain\Repository;
-use \TYPO3\CMS\Extbase\Persistence\Repository;
-
-
-class TeamSeasonRepository extends Repository {
-
-    /**
-     * @param \Balumedien\Clubms\Domain\Model\Team $team team item
-     */
-    public function findLatestSeasonOfTeam($team) {
-        // TODO: BUILD QUERY TO FIND LATEST SEASON OF A SPECIFIC TEAM
-    }
-
-}
+	
+	namespace Balumedien\Clubms\Domain\Repository;
+	
+	class TeamSeasonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+	
+	    /**
+	     * @param \Balumedien\Clubms\Domain\Model\Team $team team item
+	     */
+	    public function findLatestSeasonOfTeam($team) {
+	        // TODO: BUILD QUERY TO FIND LATEST SEASON OF A SPECIFIC TEAM
+	    }
+	
+	}
