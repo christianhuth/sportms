4c1b646140cc726e3279d56d128f5c986ea0a7e2
Christian Knell <christian.knell@balumedien.de>
2020-03-01 22:26:51 +0100

    renaming

--- Classes/Domain/Repository/SectionPositionGroupRepository.php
+++ Classes/Domain/Repository/SectionPositionGroupRepository.php
@@ -2,6 +2,6 @@
 	
 	namespace Balumedien\Clubms\Domain\Repository;
 	
-	class UrlRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+	class SectionPositionGroupRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 	
 	}
