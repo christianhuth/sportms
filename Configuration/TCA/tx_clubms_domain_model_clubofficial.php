19a916b44e83080326c16d441d78de9d40827c78
Christian Knell <christian.knell@balumedien.de>
2020-03-12 21:16:46 +0100

    wip

--- Configuration/TCA/tx_clubms_domain_model_clubofficial.php
+++ Configuration/TCA/tx_clubms_domain_model_clubofficial.php
@@ -90,7 +90,6 @@ return array(
 
         'club' => array(
             'config' => array(
-	            'foreign_table' => 'tx_clubms_domain_model_club',
                 'type' => 'passthrough',
             ),
         ),
