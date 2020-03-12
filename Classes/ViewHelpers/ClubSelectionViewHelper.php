9b90fd14365cf2cf0b96c1beb975de8751f42d0a
Christian Knell <christian.knell@balumedien.de>
2020-03-13 00:30:53 +0100

    wip

--- Classes/ViewHelpers/ClubSelectionViewHelper.php
+++ Classes/ViewHelpers/ClubSelectionViewHelper.php
@@ -4,4 +4,35 @@
 	
 	class ClubSelectionViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper {
 		
+		/**
+		 * @return void
+		 */
+		public function initializeArguments() {
+			parent::initializeArguments();
+			$this->registerArgument('clubs', 'string', 'start date', false);
+			$this->registerArgument('clubOfficials', 'string', 'start date', false);
+		}
+		
+		/**
+		 * @return \DatePeriod $dateRange
+		 * @throws \Exception
+		 */
+		public function render() {
+			
+			$startdate = new \DateTime();
+			$startdate->setTimestamp($this->arguments['startdate']);
+			
+			$enddate = new \DateTime();
+			if($this->arguments['enddate']) {
+				$enddate->setTimestamp($this->arguments['enddate']);
+			} else {
+				$enddate->setTimestamp(time());
+			}
+			$enddate->add(new \DateInterval('P1D'));
+			
+			$dateRange = $enddate->diff($startdate)->format($this->arguments['format']);
+			return $dateRange;
+			
+		}
+		
 	}
