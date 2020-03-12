1ad44389343e08923c8232d4c0efc917584178e6
Christian Knell <christian.knell@balumedien.de>
2020-03-12 21:04:54 +0100

    wip

--- Classes/Domain/Repository/ClubOfficialRepository.php
+++ Classes/Domain/Repository/ClubOfficialRepository.php
@@ -3,6 +3,12 @@
 	namespace Balumedien\Clubms\Domain\Repository;
 	
 	class ClubOfficialRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+		
+		// Order by BE sorting
+		protected $defaultOrderings = array(
+			'club' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+			'club_official_job' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
+		);
 	
 	}
 	
