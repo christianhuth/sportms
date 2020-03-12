a0720f64600553f9bf3393d7911de4495db2f177
Christian Knell <christian.knell@balumedien.de>
2020-03-12 15:34:28 +0100

    added ordering to repository

--- Classes/Domain/Repository/SeasonRepository.php
+++ Classes/Domain/Repository/SeasonRepository.php
@@ -3,9 +3,12 @@
     namespace Balumedien\Clubms\Domain\Repository;
 
     class SeasonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-
-
-
+	   
+	    // Order by BE sorting
+	    protected $defaultOrderings = array(
+		    'season_name' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
+	    );
+	
     }
 
 ?>
