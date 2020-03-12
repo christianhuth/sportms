e6d992e77dd84e3388709a6a2990709c56443e46
Christian Knell <christian.knell@balumedien.de>
2020-03-12 21:07:32 +0100

    wip

--- Classes/Domain/Repository/ClubOfficialRepository.php
+++ Classes/Domain/Repository/ClubOfficialRepository.php
@@ -2,7 +2,11 @@
 	
 	namespace Balumedien\Clubms\Domain\Repository;
 	
-	class ClubOfficialRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+	use TYPO3\CMS\Core\Utility\GeneralUtility;
+	use TYPO3\CMS\Core\Database\ConnectionPool;
+	use TYPO3\CMS\Extbase\Persistence;
+	
+	class ClubOfficialRepository extends Repository {
 		
 		// Order by BE sorting
 		protected $defaultOrderings = array(
@@ -10,6 +14,12 @@
 			'club_official_job' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
 		);
 	
+		public function findAll($clubs = null, $clubOfficialJobs = null) {
+			$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_clubms_domain_model_clubofficial');
+			$queryBuilder->select('*');
+			return $queryBuilder->execute();
+		}
+		
 	}
 	
 ?>
