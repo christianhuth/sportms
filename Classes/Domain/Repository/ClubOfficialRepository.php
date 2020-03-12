3990f12be1fccc4e59fd21b9f4aff463ada7c128
Christian Knell <christian.knell@balumedien.de>
2020-03-12 21:08:17 +0100

    wip

--- Classes/Domain/Repository/ClubOfficialRepository.php
+++ Classes/Domain/Repository/ClubOfficialRepository.php
@@ -4,7 +4,7 @@
 	
 	use TYPO3\CMS\Core\Utility\GeneralUtility;
 	use TYPO3\CMS\Core\Database\ConnectionPool;
-	use TYPO3\CMS\Extbase\Persistence;
+	use TYPO3\CMS\Extbase\Persistence\Repository;
 	
 	class ClubOfficialRepository extends Repository {
 		
