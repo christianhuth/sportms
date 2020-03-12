43b3bb2ae7cb78acd0bc70f8fd33581d5023e2ef
Christian Knell <christian.knell@balumedien.de>
2020-03-12 21:30:42 +0100

    wip

--- Classes/Domain/Repository/ClubOfficialRepository.php
+++ Classes/Domain/Repository/ClubOfficialRepository.php
@@ -10,7 +10,7 @@
 		
 		// Order by BE sorting
 		protected $defaultOrderings = array(
-			'club' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+			'club.name' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
 			'club_official_job' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
 		);
 	
