2b19a7a242cad566dfc69dccce85cb83e66f460b
Christian Knell <christian.knell@balumedien.de>
2020-03-15 23:42:38 +0100

    wip

--- Classes/ViewHelpers/ZodiacSignViewHelper.php
+++ Classes/ViewHelpers/ZodiacSignViewHelper.php
@@ -16,8 +16,20 @@
 		 * @return string
 		 */
 		public function render() {
-			$zodiacSign = $this->arguments['zodiacSign'];
-			return $zodiacSign;
+			switch($this->arguments['zodiacSign']) {
+				case 1: return "Wassermann";
+				case 2: return "Fische";
+				case 3: return "Widder";
+				case 4: return "Stier";
+				case 5: return "Zwillinge";
+				case 6: return "Krebs";
+				case 7: return "Löwe";
+				case 8: return "Jungfrau";
+				case 9: return "Waage";
+				case 10: return "Skorpion";
+				case 11: return "Schütze";
+				case 12: return "Steinbock";
+			}
 		}
 		
 	}
