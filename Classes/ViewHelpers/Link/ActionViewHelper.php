9ce196aa43bf93f655acdf392a4a2adca3e0fd3b
Christian Knell <christian.knell@balumedien.de>
2020-03-15 21:05:26 +0100

    wip

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -108,7 +108,6 @@
 			parent::initializeArguments();
 			$this->initSettings();
 			$this->initListOfClubMsDomainModels();
-			\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->getListOfClubMsDomainModels(), 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 			foreach($this->getListOfClubMsDomainModels() as $clubMsDomainModel) {
 				$this->registerArgument($clubMsDomainModel, '\Balumedien\Clubms\Domain\Model\\' . $clubMsDomainModel, strtolower($clubMsDomainModel) . ' to show', false);
 			}
@@ -118,6 +117,7 @@
 					break;
 				}
 			}
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->getClubMsDomainModel(), 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 		}
 		
 		# Needed so we can fill $this->getSettings()
