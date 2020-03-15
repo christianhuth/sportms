d56c3d2c56bc9e1bbcc13da702c20a97056aa824
Christian Knell <christian.knell@balumedien.de>
2020-03-15 20:09:12 +0100

    wip

---
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+	
+	namespace Balumedien\Clubms\ViewHelpers\Link;
+	
+	class ActionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\ActionViewHelper {
+		
+		/**
+		 * Arguments initialization
+		 */
+		public function initializeArguments() {
+			parent::initializeArguments();
+		}
+		
+		/**
+		 * @return string Rendered link
+		 */
+		public function render() {
+			$action = $this->arguments['action'];
+			$controller = $this->arguments['controller'];
+			$extensionName = $this->arguments['extensionName'];
+			$pluginName = $this->arguments['pluginName'];
+			$pageUid = (int) $this->arguments['pageUid'] ? : NULL;
+			$pageType = (int) $this->arguments['pageType'];
+			$noCache = (bool) $this->arguments['noCache'];
+			$noCacheHash = (bool) $this->arguments['noCacheHash'];
+			$section = (string) $this->arguments['section'];
+			$format = (string) $this->arguments['format'];
+			$linkAccessRestrictedPages = (bool) $this->arguments['linkAccessRestrictedPages'];
+			$additionalParams = (array) $this->arguments['additionalParams'];
+			$absolute = (bool) $this->arguments['absolute'];
+			$addQueryString = (bool) $this->arguments['addQueryString'];
+			$argumentsToBeExcludedFromQueryString = (array) $this->arguments['argumentsToBeExcludedFromQueryString'];
+			$addQueryStringMethod = $this->arguments['addQueryStringMethod'];
+			$parameters = $this->arguments['arguments'];
+			$uriBuilder = $this->renderingContext->getControllerContext()->getUriBuilder();
+			$uri = $uriBuilder->reset()->setTargetPageUid($pageUid)->setTargetPageType($pageType)->setNoCache($noCache)->setUseCacheHash(!$noCacheHash)->setSection($section)->setFormat($format)->setLinkAccessRestrictedPages($linkAccessRestrictedPages)->setArguments($additionalParams)->setCreateAbsoluteUri($absolute)->setAddQueryString($addQueryString)->setArgumentsToBeExcludedFromQueryString($argumentsToBeExcludedFromQueryString)->setAddQueryStringMethod($addQueryStringMethod)->uriFor($action, $parameters, $controller, $extensionName, $pluginName);
+			$this->tag->addAttribute('href', $uri);
+			$this->tag->setContent($this->renderChildren());
+			$this->tag->forceClosingTag(TRUE);
+			return $this->tag->render();
+		}
+		
+	}
