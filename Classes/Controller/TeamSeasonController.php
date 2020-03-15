56ce74bc4fb5d88ba3f835e85bee2358a81a1588
Christian Knell <christian.knell@balumedien.de>
2020-03-15 19:16:14 +0100

    wip

--- Classes/Controller/TeamSeasonController.php
+++ Classes/Controller/TeamSeasonController.php
@@ -1,67 +1,42 @@
 <?php
-
-namespace Balumedien\Clubms\Controller;
-
-/***************************************************************
- *
- *  Copyright notice
- *
- *  (c) 2015
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
-/**
- * TeamSeasonController
- */
-class TeamSeasonController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
-
-    /**
-     * @var \Balumedien\Clubms\Domain\Repository\TeamSeasonRepository
-     * @TYPO3\CMS\Extbase\Annotation\Inject
-     */
-    protected $teamSeasonRepository;
-
-    /**
-     * @return void
-     */
-    public function listAction() {
-        $teamSeasons = $this->teamSeasonRepository->findAll();
-        $this->view->assign('teamSeasons', $teamSeasons);
-    }
-
-    /**
-     * @param \Balumedien\Clubms\Domain\Model\Team $team team item
-     * @param \Balumedien\Clubms\Domain\Model\Season $season season item
-     */
-    public function showAction(\Balumedien\Clubms\Domain\Model\TeamSeason $teamSeason = null) {
-	    if($teamSeason === null) {
-		    if($this->settings['single']['teamseason']) {
-			    $teamSeasonUid = $this->settings['single']['teamseason'];
-			    $teamSeason = $this->teamSeasonRepository->findByUid($teamSeasonUid);
-		    }
-	    }
-	    if($teamSeason != null) {
-		    $this->view->assign('teamSeason', $teamSeason);
-	    } else {
-	    	// TODO: ERROR HANDLING
-	    }
-    }
-
-}
+	
+	namespace Balumedien\Clubms\Controller;
+	
+	/**
+	 * TeamSeasonController
+	 */
+	class TeamSeasonController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+		
+		/**
+		 * @var \Balumedien\Clubms\Domain\Repository\TeamSeasonRepository
+		 * @TYPO3\CMS\Extbase\Annotation\Inject
+		 */
+		protected $teamSeasonRepository;
+		
+		/**
+		 * @return void
+		 */
+		public function listAction() {
+			$teamSeasons = $this->teamSeasonRepository->findAll();
+			$this->view->assign('teamSeasons', $teamSeasons);
+		}
+		
+		/**
+		 * @param \Balumedien\Clubms\Domain\Model\Team $team team item
+		 * @param \Balumedien\Clubms\Domain\Model\Season $season season item
+		 */
+		public function showAction(\Balumedien\Clubms\Domain\Model\TeamSeason $teamSeason = NULL) {
+			if($teamSeason === NULL) {
+				if($this->settings['single']['teamseason']) {
+					$teamSeasonUid = $this->settings['single']['teamseason'];
+					$teamSeason = $this->teamSeasonRepository->findByUid($teamSeasonUid);
+				}
+			}
+			if($teamSeason != NULL) {
+				$this->view->assign('teamSeason', $teamSeason);
+			} else {
+				// TODO: ERROR HANDLING
+			}
+		}
+		
+	}
