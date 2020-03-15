565e5852aafdc756b83e6db94bb8138c63bc1de7
Christian Knell <christian.knell@balumedien.de>
2020-03-15 19:25:10 +0100

    wip

--- Classes/Controller/CompetitionController.php
+++ Classes/Controller/CompetitionController.php
@@ -1,60 +1,35 @@
 <?php
-
-namespace Balumedien\Clubms\Controller;
-
-/***************************************************************
- *
- *  Copyright notice
- *
- *  (c) 2015
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
-/**
- * CompetitionController
- */
-class CompetitionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
-
-	/**
-	 * @var \Balumedien\Clubms\Domain\Repository\CompetitionRepository
-	 * @TYPO3\CMS\Extbase\Annotation\Inject
-	 */
-	protected $competitionRepository;
+	
+	namespace Balumedien\Clubms\Controller;
 	
 	/**
-	 * @return void
+	 * CompetitionController
 	 */
-	public function listAction() {
-		$competitions = $this->competitionRepository->findAll();
-		$this->view->assign('competitions', $competitions);
+	class CompetitionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+		
+		/**
+		 * @var \Balumedien\Clubms\Domain\Repository\CompetitionRepository
+		 * @TYPO3\CMS\Extbase\Annotation\Inject
+		 */
+		protected $competitionRepository;
+		
+		/**
+		 * @return void
+		 */
+		public function listAction() {
+			$competitions = $this->competitionRepository->findAll();
+			$this->view->assign('competitions', $competitions);
+		}
+		
+		/**
+		 * @param \Balumedien\Clubms\Domain\Model\Competition $competition competition item
+		 */
+		public function showAction(\Balumedien\Clubms\Domain\Model\Competition $competition = NULL) {
+			if($competition === NULL) {
+				$competitionUid = $this->settings['competition']['uid'];
+				$competition = $this->competitionRepository->findByUid($competitionUid);
+			}
+			$this->view->assign('competition', $competition);
+		}
+		
 	}
-
-    /**
-     * @param \Balumedien\Clubms\Domain\Model\Competition $competition competition item
-     */
-    public function showAction(\Balumedien\Clubms\Domain\Model\Competition $competition = null) {
-        if($competition === null) {
-            $competitionUid = $this->settings['single']['competition'];
-            $competition = $this->competitionRepository->findByUid($competitionUid);
-        }
-        $this->view->assign('competition', $competition);
-    }
-
-}
