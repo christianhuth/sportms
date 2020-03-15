5144d3d24acb1d8a52889fd56025cc91553b2b3f
Christian Knell <christian.knell@balumedien.de>
2020-03-16 00:16:05 +0100

    wip

--- Classes/Domain/Repository/SectionRepository.php
+++ Classes/Domain/Repository/SectionRepository.php
@@ -4,6 +4,11 @@
 	
 	class SectionRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		
+		// Order by BE sorting
+		protected $defaultOrderings = array(
+			'label' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
+		);
+		
 	}
 	
 ?>
