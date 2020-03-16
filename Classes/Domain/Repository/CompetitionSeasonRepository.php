1d06ef1da1e368247a4a1ebe8c080182cc3fb3ea
Christian Knell <christian.knell@balumedien.de>
2020-03-16 15:40:35 +0100

    wip

--- Classes/Domain/Repository/CompetitionSeasonRepository.php
+++ Classes/Domain/Repository/CompetitionSeasonRepository.php
@@ -4,6 +4,33 @@
 	
 	class CompetitionSeasonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		
+		public function findAll($competitionsFilter = null, $competitionTypesFilter = null, $sectionsFilter = null, $sectionAgeGroupsFilter = null, $sectionAgeLevelsFilter = null, $seasonsFilter = null) {
+			$query = $this->createQuery();
+			$constraints = [];
+			if($competitionsFilter) {
+				$constraints[] = $query->in('competition', explode(',', $competitionsFilter));
+			}
+			if($competitionTypesFilter) {
+				$constraints[] = $query->in('competition.competitionType', explode(',', $competitionTypesFilter));
+			}
+			if($sectionsFilter) {
+				$constraints[] = $query->in('competition.section', explode(',', $sectionsFilter));
+			}
+			if($sectionAgeGroupsFilter) {
+				$constraints[] = $query->in('competition.sectionAgeGroup', explode(',', $sectionAgeGroupsFilter));
+			}
+			if($sectionAgeLevelsFilter) {
+				$constraints[] = $query->in('competition.sectionAgeLevel', explode(',', $sectionAgeLevelsFilter));
+			}
+			if($seasonsFilter) {
+				$constraints[] = $query->in('season', explode(',', $seasonsFilter));
+			}
+			if($constraints) {
+				$query->matching($query->logicalAnd($constraints));
+			}
+			return $query->execute();
+		}
+		
 	}
 	
 ?>
