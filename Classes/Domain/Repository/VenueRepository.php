1e5f1ea2136e75f7025c3cae9c00cca02de06c30
Christian Knell <christian.knell@balumedien.de>
2020-03-16 13:29:16 +0100

    wip

--- Classes/Domain/Repository/VenueRepository.php
+++ Classes/Domain/Repository/VenueRepository.php
@@ -3,5 +3,20 @@
 	namespace Balumedien\Clubms\Domain\Repository;
 	
 	class VenueRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+		
+		public function findAll($clubsFilter = null, $withClubOnly = false) {
+			$query = $this->createQuery();
+			$constraints = [];
+			if($clubsFilter) {
+				$constraints[] = $query->in('club', explode(",", $clubsFilter));
+			}
+			if($withClubOnly) {
+				$constraints[] = $query->logicalNot($query->equals('club', NULL));
+			}
+			if($constraints) {
+				$query->matching($query->logicalAnd($constraints));
+			}
+			return $query->execute();
+		}
 	
 	}
