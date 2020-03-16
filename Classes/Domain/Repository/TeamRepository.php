70988f5d5c56e21212612ceef66e47bd88897034
Christian Knell <christian.knell@balumedien.de>
2020-03-16 13:48:14 +0100

    wip

--- Classes/Domain/Repository/TeamRepository.php
+++ Classes/Domain/Repository/TeamRepository.php
@@ -3,5 +3,23 @@
 	namespace Balumedien\Clubms\Domain\Repository;
 
 	class TeamRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+		
+		public function findAll($teamsFilter = null, $sectionsFilter = null, $clubsFilter = null, $sectionAgeGroupsFilter = null, $sectionAgeLevelsFilter = null) {
+			$query = $this->createQuery();
+			$constraints = [];
+			if($teamsFilter) {
+				$constraints[] = $query->in('uid', explode(',', $teamsFilter));
+			}
+			if($sectionsFilter) {
+				$constraints[] = $query->in('section', explode(',', $sectionsFilter));
+			}
+			if($clubsFilter) {
+				$constraints[] = $query->in('club', explode(',', $clubsFilter));
+			}
+			if($constraints) {
+				$query->matching($query->logicalAnd($constraints));
+			}
+			return $query->execute();
+		}
 
 	}
