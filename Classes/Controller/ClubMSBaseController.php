755489bc2eda6ed266fc4eb1a4e7245b8f62eb54
Christian Knell <christian.knell@balumedien.de>
2020-03-17 21:42:09 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -29,6 +29,7 @@
 		}
 		
 		protected function getFilter($model, $useSelected) {
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->settings[$model][$model . 's'], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 			return ($useSelected) ? $this->settings[$model]['selected'] : $this->settings[$model][$model . 's'];
 		}
 		
