a4a131b0ef5f141c951b0cc8d859b1452a1e921f
Christian Knell <christian.knell@balumedien.de>
2020-03-18 00:44:59 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -47,9 +47,7 @@
 			if(is_array($setting[1])) {
 				return $this->mergeRequestWithSetting($request, $setting[1], array_push($position, $setting[0]));
 			} else if($this->request->hasArgument($request)) {
-				\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[0], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-				\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[1], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-				\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->request->getArgument($request), 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+				\TYPO3\CMS\Core\Utility\DebugUtility::debug($position, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 				$this->settings[$position][$setting[0]][$setting[1]] = $this->request->getArgument($request);
 			}
 		}
