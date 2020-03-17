55307a179bf50ff726cee763f595a35041d71a66
Christian Knell <christian.knell@balumedien.de>
2020-03-17 13:46:29 +0100

    wip

---
+++ Classes/ViewHelpers/Club/SelectViewHelper.php
@@ -0,0 +1,22 @@
+<?php
+	
+	namespace Balumedien\Clubms\ViewHelpers;
+	
+	class SelectViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper {
+		
+		/**
+		 * @return void
+		 */
+		public function initializeArguments() {
+			parent::initializeArguments();
+		}
+		
+		/**
+		 * @return \DatePeriod $dateRange
+		 * @throws \Exception
+		 */
+		public function render() {
+			return null;
+		}
+		
+	}
