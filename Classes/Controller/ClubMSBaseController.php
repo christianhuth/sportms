0195c271eb9033e1e0afe0037103f65de0e8acf8
Christian Knell <christian.knell@balumedien.de>
2020-03-18 00:41:38 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -44,17 +44,15 @@
 		}
 		
 		protected function mergeRequestWithSetting(string $request, array $setting, array &$settings = null) {
-			if(is_array($setting)) {
-				if(is_array($setting[1])) {
-					return $this->mergeRequestWithSetting($request, $setting[1], $this->settings[$setting[0]]);
-				} else if($this->request->hasArgument($request)) {
-					\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[0], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-					\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[1], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-					\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->request->getArgument($request), 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-					$settings[$setting[0]][$setting[1]] = $this->request->getArgument($request);
-				}
-			} else {
+			if(is_array($setting[1])) {
+				return $this->mergeRequestWithSetting($request, $setting[1], $this->settings[$setting[0]]);
+			} else if($this->request->hasArgument($request)) {
+				\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[0], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+				\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[1], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+				\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->request->getArgument($request), 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+				$settings[$setting[0]][$setting[1]] = $this->request->getArgument($request);
 			}
+			$this->settings = $settings;
 		}
 		
 		protected function determineShowViews($model, $listOfPossibleShowViews): void {
