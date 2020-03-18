5c069fc487957443d87446d5068a8ca9c74d9530
Christian Knell <christian.knell@balumedien.de>
2020-03-18 15:56:47 +0100

    wip

--- Classes/Controller/CompetitionSeasonController.php
+++ Classes/Controller/CompetitionSeasonController.php
@@ -16,9 +16,29 @@
 		protected $competitionSeasonRepository;
 		
 		/**
+		 * Initializes the controller before invoking an action method.
+		 * Use this method to solve tasks which all actions have in common.
+		 */
+		public function initializeAction() {
+			$this->mapRequestsToSettings();
+		}
+		
+		/**
+		 * Use this method to solve tasks which all actions have in common, when VIEW-Context is needed
+		 */
+		public function initializeActions() {
+			$listOfPossibleShowViews = 'index,games,teams';
+			$this->determineShowView($this->model);
+			$this->determineShowViews($this->model, $listOfPossibleShowViews);
+			$this->determineShowNavigationViews($this->model, $listOfPossibleShowViews);
+			$this->view->assign('settings', $this->settings);
+		}
+		
+		/**
 		 * @return void
 		 */
 		public function listAction() {
+			$this->initializeActions();
 			$competitionsFilter = $this->settings['competition']['competitions'];
 			$competitionTypesFilter = $this->settings['competition']['competitionTypes'];
 			$sectionsFilter = $this->settings['section']['sections'];
@@ -33,6 +53,7 @@
 		 * @param \Balumedien\Clubms\Domain\Model\CompetitionSeason $competitionSeason
 		 */
 		public function showAction(\Balumedien\Clubms\Domain\Model\CompetitionSeason $competitionSeason = NULL) {
+			$this->initializeActions();
 			if($competitionSeason === NULL) {
 				$competitionSeasonUid = $this->settings['competitionSeason']['uid'];
 				$competitionSeason = $this->competitionSeasonRepository->findByUid($competitionSeasonUid);
