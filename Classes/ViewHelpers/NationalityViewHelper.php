109d08a48cc3395f3371812f9ab6f03b70a35ead
Christian Knell <christian.knell@balumedien.de>
2020-03-18 11:33:34 +0100

    wip

--- Classes/ViewHelpers/NationalityViewHelper.php
+++ Classes/ViewHelpers/NationalityViewHelper.php
@@ -32,18 +32,8 @@
 		 */
 		public function render(): array {
 			$key = $this->arguments['key'];
-			$value = $this->arguments['value'];
-			$sortbyField = $this->arguments['sortbyField'];
-			$sorting = $this->arguments['sorting'];
-			
-			$countries = $this->countryRepository->findAllOrderedBy($sortbyField, $sorting);
-			$countriesArray = [];
-			foreach($countries as $country) {
-				/** @var $country \SJBR\StaticInfoTables\Domain\Model\Country */
-				$countriesArray[ObjectAccess::getProperty($country, $key)] = ObjectAccess::getProperty($country, $value);
-			}
-			
-			return $countriesArray;
+			$country = $this->countryRepository->findByUid($key);
+			return $country;
 		}
 		
 		
