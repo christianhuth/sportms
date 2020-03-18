a4d5b73985283f5fb8c270c9a25ec6449a4a339a
Christian Knell <christian.knell@balumedien.de>
2020-03-18 16:11:37 +0100

    wip

--- Classes/Domain/Repository/SeasonRepository.php
+++ Classes/Domain/Repository/SeasonRepository.php
@@ -9,6 +9,18 @@
 		    'season_name' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
 	    );
 	
+	    public function findAll($seasonsFilter = null) {
+		    $query = $this->createQuery();
+		    $constraints = [];
+		    if($seasonsFilter) {
+			    $constraints[] = $query->in('uid', explode(',', $seasonsFilter));
+		    }
+		    if($constraints) {
+			    $query->matching($query->logicalAnd($constraints));
+		    }
+		    return $query->execute();
+	    }
+	
     }
 
 ?>
