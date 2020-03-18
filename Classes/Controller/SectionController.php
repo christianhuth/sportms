343c3b934ecf1a27e30fc6eeaed62e50711922f0
Christian Knell <christian.knell@balumedien.de>
2020-03-18 16:15:07 +0100

    wip

--- Classes/Controller/SectionController.php
+++ Classes/Controller/SectionController.php
@@ -16,10 +16,30 @@
 		protected $sectionRepository;
 		
 		/**
+		 * Initializes the controller before invoking an action method.
+		 * Use this method to solve tasks which all actions have in common.
+		 */
+		public function initializeAction() {
+			$this->mapRequestsToSettings();
+		}
+		
+		/**
+		 * Use this method to solve tasks which all actions have in common, when VIEW-Context is needed
+		 */
+		public function initializeActions() {
+			$listOfPossibleShowViews = 'index';
+			$this->determineShowView($this->model);
+			$this->determineShowViews($this->model, $listOfPossibleShowViews);
+			$this->determineShowNavigationViews($this->model, $listOfPossibleShowViews);
+			$this->view->assign('settings', $this->settings);
+		}
+		
+		/**
 		 * @return void
 		 */
 		public function listAction() {
-			$sections = $this->sectionRepository->findAll();
+			$this->initializeActions();
+			$sections = $this->sectionRepository->findAll($this->getSectionsFilter());
 			$this->view->assign('sections', $sections);
 		}
 		
@@ -27,6 +47,7 @@
 		 * @param \Balumedien\Clubms\Domain\Model\Section $game
 		 */
 		public function showAction(\Balumedien\Clubms\Domain\Model\Section $section = NULL) {
+			$this->initializeActions();
 			$this->view->assign('section', $section);
 		}
 		
