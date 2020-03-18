3633d44c0acf25c475071bec3feb46dee949cce0
Christian Knell <christian.knell@balumedien.de>
2020-03-18 02:59:58 +0100

    wip

--- Classes/Controller/ClubController.php
+++ Classes/Controller/ClubController.php
@@ -18,14 +18,6 @@
 		protected $clubRepository = NULL;
 		
 		/**
-		 * @var \Balumedien\Clubms\Domain\Repository\ClubOfficialRepository
-		 * @TYPO3\CMS\Extbase\Annotation\Inject
-		 */
-		protected $clubOfficialRepository = NULL;
-		
-		
-		
-		/**
 		 * Initializes the controller before invoking an action method.
 		 * Use this method to solve tasks which all actions have in common.
 		 */
@@ -64,32 +56,6 @@
 			$this->initializeActions();
 			if($club === NULL) {
 				// TODO: CHECK IF SETTINGS IS SET ELSE DIE
-				$clubUid = $this->settings['club']['uid'];
-				$club = $this->clubRepository->findByUid($clubUid);
-			}
-			$this->view->assign('club', $club);
-		}
-		
-		/**
-		 * @param \Balumedien\Clubms\Domain\Model\Club|null $club
-		 */
-		public function officialsAction(\Balumedien\Clubms\Domain\Model\Club $club = NULL) {
-			
-			$clubs = $this->settings['club']['clubs'];
-			$jobs = $this->settings['club']['officials']['jobs'];
-			$currentOfficialsOnly = $this->settings['club']['officials']['currentOfficialsOnly'];
-			
-			$clubOfficials = $this->clubOfficialRepository->findAll($clubs, $jobs, $currentOfficialsOnly);
-			
-			$this->view->assign('clubOfficials', $clubOfficials);
-			
-		}
-		
-		/**
-		 * @param \Balumedien\Clubms\Domain\Model\Club $club club item
-		 */
-		public function sectionsAction(\Balumedien\Clubms\Domain\Model\Club $club = NULL) {
-			if($club === NULL) {
 				$clubUid = $this->settings['club']['uid'];
 				$club = $this->clubRepository->findByUid($clubUid);
 			}
