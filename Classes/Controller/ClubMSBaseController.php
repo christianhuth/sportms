8f8519799561eb74a8364b23d2bf1aca214321e6
Christian Knell <christian.knell@balumedien.de>
2020-03-18 01:12:58 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -38,6 +38,7 @@
 			$listOfMappings[] = ['showView', array('club' => array('showView' => array('current'=>'')))];
 			foreach($listOfMappings as $mapping) {
 				if($this->request->hasArgument($mapping[0])) {
+					\TYPO3\CMS\Core\Utility\DebugUtility::debug($mapping[1], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 					$mappedRequest = $this->mapRequestToSetting($mapping[0], $mapping[1]);
 				}
 			}
