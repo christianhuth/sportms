46de9fe0aa74501593fc6f395454ead2283fced8
Christian Knell <christian.knell@balumedien.de>
2020-03-18 16:09:43 +0100

    wip

--- Classes/Domain/Repository/PersonRepository.php
+++ Classes/Domain/Repository/PersonRepository.php
@@ -3,8 +3,18 @@
     namespace Balumedien\Clubms\Domain\Repository;
 
     class PersonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-
-
+	
+	    public function findAll($personsFilter = null) {
+		    $query = $this->createQuery();
+		    $constraints = [];
+		    if($personsFilter) {
+			    $constraints[] = $query->in('uid', explode(',', $personsFilter));
+		    }
+		    if($constraints) {
+			    $query->matching($query->logicalAnd($constraints));
+		    }
+		    return $query->execute();
+	    }
 
     }
 
