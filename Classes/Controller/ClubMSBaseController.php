65b9be857dbc583c2742bd243811be307c373025
Christian Knell <christian.knell@balumedien.de>
2020-03-18 01:11:45 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -32,61 +32,25 @@
 			return ($useSelected) ? $this->settings[$model]['selected'] : $this->settings[$model][$model . 's'];
 		}
 		
-		protected function mergeRequestsWithSettings(): void {
+		protected function mapRequestsToSettings(): void {
 			$listOfMappings = array();
 			$listOfMappings[] = ['selectClub', array('club'=>array('selected'=>''))];
 			$listOfMappings[] = ['showView', array('club' => array('showView' => array('current'=>'')))];
 			foreach($listOfMappings as $mapping) {
 				if($this->request->hasArgument($mapping[0])) {
-					array_push($mapping[1], $this->request->getArgument($mapping[0]));
-					\TYPO3\CMS\Core\Utility\DebugUtility::debug($mapping[1], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-					array_push($this->settings[], $mapping[1]);
+					$mappedRequest = $this->mapRequestToSetting($mapping[0], $mapping[1]);
 				}
-				#if(count($mapping) === 2) {
-				#	$this->mergeRequestWithSetting($mapping[0], $mapping[1]);
-				#}
 			}
 		}
 		
-		/*
-		* Inserts a new key/value after the key in the array.
-		*
-		* @param $key
-		*   The key to insert after.
-		* @param $array
-		*   An array to insert in to.
-		* @param $new_key
-		*   The key to insert.
-		* @param $new_value
-		*   An value to insert.
-		*
-		* @return
-		*   The new array if the key exists, FALSE otherwise.
-		*
-		* @see array_insert_before()
-		*/
-		function array_insert_after($key, array &$array, $new_key, $new_value) {
-			if (array_key_exists($key, $array)) {
-				$new = array();
-				foreach ($array as $k => $value) {
-					$new[$k] = $value;
-					if ($k === $key) {
-						$new[$new_key] = $new_value;
-					}
-				}
-				return $new;
-			}
-			return FALSE;
-		}
-		
-		protected function mergeRequestWithSetting(string $request, array $setting, array $resultArray = array()) {
-			$position[$setting[0]] = array();
+		protected function mapRequestToSetting(string $request, array $setting, array $resultArray = array()): array {
+			array_push($resultArray, $setting[0]);
 			\TYPO3\CMS\Core\Utility\DebugUtility::debug($setting[0], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-			\TYPO3\CMS\Core\Utility\DebugUtility::debug($position, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($resultArray, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 			if(is_array($setting[1])) {
-				return $this->mergeRequestWithSetting($request, $setting[1], $position);
+				return $this->mapRequestToSetting($request, $setting[1], $resultArray);
 			} else if($this->request->hasArgument($request)) {
-				$this->settings[$position][$setting[1]] = $this->request->getArgument($request);
+				#$this->settings[$position][$setting[1]] = $this->request->getArgument($request);
 			}
 		}
 		
