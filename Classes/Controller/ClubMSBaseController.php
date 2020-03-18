97d1e958c093119a1e5544444a8bc4c98f2fcbdf
Christian Knell <christian.knell@balumedien.de>
2020-03-18 02:04:44 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -57,32 +57,6 @@
 			}
 		}
 		
-		protected function mapRequestToSetting(string $requestValue, array &$positionInSettings, array &$settings) {
-			
-			$currentPositionByKey = array_key_first($positionInSettings);
-			if(is_array($positionInSettings[$currentPositionByKey])) {
-				\TYPO3\CMS\Core\Utility\DebugUtility::debug($settings, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-				$this->mapRequestToSetting($requestValue, $positionInSettings[$currentPositionByKey], $settings[$currentPositionByKey]);
-			} else {
-				$settings[$currentPositionByKey] = $requestValue;
-			}
-			
-			
-			
-			
-			
-			/*
-			if(is_array(array_pop(array_reverse($setting)))) {
-			} else {
-				if(array_key_exists($setting)) {
-				
-				}
-				$key = array_key_first($setting);
-				$setting[$key] = $this->request->getArgument($request);
-			}
-			*/
-		}
-		
 		protected function determineShowViews($model, $listOfPossibleShowViews): void {
 			foreach(explode(',', $listOfPossibleShowViews) AS $showView) {
 				$this->settings[$model]['showView'][$showView] = ($this->settings[$model]['showViews']) ? strpos($this->settings[$model]['showViews'], $showView) !== FALSE : TRUE;
@@ -90,7 +64,7 @@
 		}
 		
 		protected function determineShowView($model): void {
-			$this->settings[$model]['showView']['current'] = ($this->settings[$model]['showView']['current']) ? : ($this->settings[$model]['showViews']) ? explode(',', $this->settings[$model]['showViews'])[0] : 'index';
+			#$this->settings[$model]['showView']['current'] = ($this->settings[$model]['showView']['current']) ? : ($this->settings[$model]['showViews']) ? explode(',', $this->settings[$model]['showViews'])[0] : 'index';
 		}
 		
 	}
