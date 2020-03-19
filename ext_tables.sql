ec0e0a6829bcd12b8d6643a615e04316282c00f0
Christian Knell <christian.knell@balumedien.de>
2020-03-19 16:10:32 +0100

    wip

--- ext_tables.sql
+++ ext_tables.sql
@@ -1545,7 +1545,6 @@ CREATE TABLE tx_clubms_domain_model_team (
 	dummy tinyint(4) unsigned DEFAULT '0' NOT NULL,
     detail_link tinyint(4) unsigned DEFAULT '1' NOT NULL,
 	team_seasons int(11) DEFAULT NULL,
-    detail_link tinyint(4) unsigned DEFAULT '1' NOT NULL,
     slug varchar(2048) DEFAULT '',
 	
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
