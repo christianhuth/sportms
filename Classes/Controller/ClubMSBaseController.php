2a14b44ba70882e739986a989c29cf9aada2c87a
Christian Knell <christian.knell@balumedien.de>
2020-03-19 21:24:57 +0100

    wip

--- Classes/Controller/ClubMSBaseController.php
+++ Classes/Controller/ClubMSBaseController.php
@@ -69,6 +69,7 @@
 				$model = strtolower($this->request->getArgument('controller'));
 				$this->settings[$model]['showView']['current'] = $this->request->getArgument('showView');
 			}
+			\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->settings, 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
 		}
 		
 		protected function determineShowViews($model, $listOfPossibleShowViews): void {
