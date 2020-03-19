cf4e92932a09af1d50dcd6eb4ddca0f4c4691731
Christian Knell <christian.knell@balumedien.de>
2020-03-19 23:27:09 +0100

    wip

--- Classes/Domain/Repository/SectionAgeGroupRepository.php
+++ Classes/Domain/Repository/SectionAgeGroupRepository.php
@@ -16,7 +16,16 @@
 		 */
 		public function findAllByUidsAndSection(string $uids, int $section) {
 			$query = $this->createQuery();
-			$query->matching($query->logicalAnd($query->in('uid', explode(',', $uids)), $query->equals('section', $section)));
+			$constraints = [];
+			if($uids) {
+				$constraints[] = $query->in('uid', explode(',', $uids));
+			}
+			if($section) {
+				$constraints[] = $query->equals('section', $section);
+			}
+			if($constraints) {
+				$query->matching($query->logicalAnd($constraints));
+			}
 			# Folgender Schnippel debugged eine Query, die in der Variable $query gespeichert ist
 			$queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
 			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
