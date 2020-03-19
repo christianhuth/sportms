9d6efe17f33d69e64dd57f4b460889bbea81145d
Christian Knell <christian.knell@balumedien.de>
2020-03-19 23:25:41 +0100

    wip

--- Classes/Domain/Repository/SectionAgeGroupRepository.php
+++ Classes/Domain/Repository/SectionAgeGroupRepository.php
@@ -4,7 +4,9 @@
 	
 	class SectionAgeGroupRepository extends ClubMSBaseRepository {
 		
-		protected $defaultOrderings = array('sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,);
+		protected $defaultOrderings = array(
+			'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+		);
 		
 		/**
 		 * @param string $uids
@@ -14,7 +16,7 @@
 		 */
 		public function findAllByUidsAndSection(string $uids, int $section) {
 			$query = $this->createQuery();
-			$query->matching($query->logicalAnd($query->in('uid', explode(',', $uids))), $query->equals('section', $section));
+			$query->matching($query->logicalAnd($query->in('uid', explode(',', $uids)), $query->equals('section', $section)));
 			# Folgender Schnippel debugged eine Query, die in der Variable $query gespeichert ist
 			$queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
 			\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
