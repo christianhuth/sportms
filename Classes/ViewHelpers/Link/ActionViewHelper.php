920cc66144ba265a5bc6fab50476bb5dcaa1ddc5
Christian Knell <christian.knell@balumedien.de>
2020-03-19 15:34:38 +0100

    wip

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -139,7 +139,7 @@
 					break;
 				}
 			}
-			switch($this->arguments[$this->clubMsDomainModel]->getType()) {
+			switch(getType($this->arguments[$this->clubMsDomainModel])) {
 				case "Club": if($this->arguments[$this->clubMsDomainModel]->isDetailLink()) { return null; } break;
 				case "Balumedien\Clubms\Domain\Model\Club": if($this->arguments[$this->clubMsDomainModel]->isDetailLink()) { return null; } break;
 			}
