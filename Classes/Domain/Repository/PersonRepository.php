5dac1887744d9dd3e574d4932a22be6215663fe3
Christian Knell <christian.knell@balumedien.de>
2020-03-19 22:17:53 +0100

    wip

--- Classes/Domain/Repository/PersonRepository.php
+++ Classes/Domain/Repository/PersonRepository.php
@@ -3,6 +3,12 @@
     namespace Balumedien\Clubms\Domain\Repository;
 
     class PersonRepository extends ClubMSBaseRepository {
+	
+	    // Order by BE sorting
+	    protected $defaultOrderings = array(
+		    'lastname' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+		    'firstname' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+	    );
 
     }
 
