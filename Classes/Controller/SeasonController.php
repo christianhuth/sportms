4a6baff035ad58139089fd9fc7a20b54695e5abb
Christian Knell <christian.knell@balumedien.de>
2020-03-19 22:16:02 +0100

    wip

--- Classes/Controller/SeasonController.php
+++ Classes/Controller/SeasonController.php
@@ -19,14 +19,14 @@
 		 * Initializes the controller before invoking an action method.
 		 * Use this method to solve tasks which all actions have in common.
 		 */
-		public function initializeAction() {
+		public function initializeAction(): void {
 			$this->mapRequestsToSettings();
 		}
 		
 		/**
 		 * Use this method to solve tasks which all actions have in common, when VIEW-Context is needed
 		 */
-		public function initializeActions() {
+		public function initializeActions(): void {
 			$listOfPossibleShowViews = 'index';
 			$this->determineShowView($this->model);
 			$this->determineShowViews($this->model, $listOfPossibleShowViews);
@@ -36,17 +36,18 @@
 		
 		/**
 		 * @return void
+		 * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
 		 */
-		public function listAction() {
+		public function listAction(): void {
 			$this->initializeActions();
-			$seasons = $this->seasonRepository->findAll($this->getSeasonsFilter());
+			$seasons = $this->seasonRepository->findAllByUids($this->getSeasonsFilter());
 			$this->view->assign('seasons', $seasons);
 		}
 		
 		/**
 		 * @param \Balumedien\Clubms\Domain\Model\Season $season
 		 */
-		public function showAction(\Balumedien\Clubms\Domain\Model\Season $season = NULL) {
+		public function showAction(\Balumedien\Clubms\Domain\Model\Season $season = NULL): void {
 			$this->initializeActions();
 			$this->view->assign('season', $season);
 		}
