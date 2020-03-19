4924c5a126faaf7745923f63b2625b8679536eb5
Christian Knell <christian.knell@balumedien.de>
2020-03-19 22:08:31 +0100

    wip

--- Classes/Domain/Repository/ClubMSBaseRepository.php
+++ Classes/Domain/Repository/ClubMSBaseRepository.php
@@ -4,6 +4,18 @@
 	
 	class ClubMSBaseRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		
-	
+		/**
+		 * @param string $uids
+		 * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+		 * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
+		 */
+		public function findAllByUids(string $uids) {
+			if(!$uids) {
+				return $this->findAll();
+			} else {
+				$query = $this->createQuery();
+				return $query->matching($query->logicalAnd($query->in('uid', explode(',', $uids))))->execute();
+			}
+		}
 		
 	}
--- Classes/Domain/Repository/ClubRepository.php
+++ Classes/Domain/Repository/ClubRepository.php
@@ -16,20 +16,6 @@
 			return $query->execute();
 		}
 		
-		/**
-		 * @param string $uids
-		 * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
-		 * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
-		 */
-		public function findAllByUids(string $uids) {
-			if(!$uids) {
-				return $this->findAll();
-			} else {
-				$query = $this->createQuery();
-				return $query->matching($query->logicalAnd($query->in('uid', explode(',', $uids))))->execute();
-			}
-		}
-		
 		public function findAllByClubSections($clubSections) {
 			$query = $this->createQuery();
 			$constraints = [];
