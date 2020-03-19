42a65e868d8f25df01d0a7f0cee8b1ee303a0fcf
Christian Knell <christian.knell@balumedien.de>
2020-03-19 22:09:08 +0100

    wip

--- Classes/Domain/Repository/ClubRepository.php
+++ Classes/Domain/Repository/ClubRepository.php
@@ -4,18 +4,6 @@
 	
 	class ClubRepository extends ClubMSBaseRepository {
 		
-		public function findAll($clubUids = null) {
-			$query = $this->createQuery();
-			$constraints = [];
-			if($clubUids) {
-				$constraints[] = $query->in('uid', explode(',', $clubUids));
-			}
-			if($constraints) {
-				$query->matching($query->logicalAnd($constraints));
-			}
-			return $query->execute();
-		}
-		
 		public function findAllByClubSections($clubSections) {
 			$query = $this->createQuery();
 			$constraints = [];
