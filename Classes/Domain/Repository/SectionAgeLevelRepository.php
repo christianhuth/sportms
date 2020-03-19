5d40dc7b0e8610ac4d1de2a4f13961d4c1d74437
Christian Knell <christian.knell@balumedien.de>
2020-03-19 23:47:05 +0100

    wip

--- Classes/Domain/Repository/SectionAgeLevelRepository.php
+++ Classes/Domain/Repository/SectionAgeLevelRepository.php
@@ -7,5 +7,26 @@
 		protected $defaultOrderings = array(
 			'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
 		);
+		
+		/**
+		 * @param string $uids
+		 * @param int $section
+		 * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+		 * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
+		 */
+		public function findAllByUidsAndSection(string $uids, int $section) {
+			$query = $this->createQuery();
+			$constraints = [];
+			if($uids) {
+				$constraints[] = $query->in('uid', explode(',', $uids));
+			}
+			if($section) {
+				$constraints[] = $query->equals('sectionAgeGroup.section', $section);
+			}
+			if($constraints) {
+				$query->matching($query->logicalAnd($constraints));
+			}
+			return $query->execute();
+		}
 	
 	}
