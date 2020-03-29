973ff89dcef42b41c1a84c03384313738e07d242
Christian Knell <christian.knell@balumedien.de>
2020-03-29 03:29:31 +0200

    wip

--- Configuration/TCA/tx_sportms_domain_model_sport.php
+++ Configuration/TCA/tx_sportms_domain_model_sport.php
@@ -73,7 +73,7 @@ return array(
 					['', 0],
 				],
 				'foreign_table' => 'tx_test_domain_model_test',
-				'foreign_table_where' => 'AND {#tx_test_domain_model_test}.{#pid}=###CURRENT_PID### AND {#tx_test_domain_model_test}.{#sys_language_uid} IN (-1,0)',
+				'foreign_table_where' => 'AND {#tx_sportms_domain_model_sport}.{#pid}=###CURRENT_PID### AND {#tx_sportms_domain_model_sport}.{#sys_language_uid} IN (-1,0)',
 			],
 		],
 		'l10n_diffsource' => [
