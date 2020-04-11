7cf9ceb3a1cdaf0f963252295bfe9d1c069934c5
Christian Knell <christian.knell@balumedien.de>
2020-04-11 15:12:36 +0200

    wip

--- ext_localconf.php
+++ ext_localconf.php
@@ -28,5 +28,22 @@
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
 		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sportms/Configuration/TSconfig/ContentElementWizard.txt">'
 	);
-
-?>
+	
+	/* ===========================================================================
+		Register Icons for the Backend
+	=========================================================================== */
+	if (TYPO3_MODE === 'BE') {
+		$icons = [
+			'ext-sportms-wizard-icon' => 'Extension.svg',
+		];
+		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+		foreach ($icons as $identifier => $filename) {
+			if (!$iconRegistry->isRegistered($identifier)) {
+				$iconRegistry->registerIcon(
+					$identifier,
+					\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+					['source' => 'EXT:sportms/Resources/Public/Icons/' . $filename]
+				);
+			}
+		}
+	}
