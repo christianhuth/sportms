ffef75cd5890ee1c9136e44f2903194172f82cc6
Christian Knell <christian.knell@balumedien.de>
2020-04-13 02:46:50 +0200

    wip

--- Configuration/TCA/tx_sportms_domain_model_competitionseason.php
+++ Configuration/TCA/tx_sportms_domain_model_competitionseason.php
@@ -18,11 +18,8 @@
 			'iconfile' => 'EXT:sportms/Resources/Public/Icons/tx_sportms_domain_model_teamseason.svg',
             'label' => '',
 			'label_userFunc' => \Balumedien\Sportms\Configuration\TCA\UserFunc\UserFunc::class . '->CompetitionSeasonLabel',
-			'languageField' => 'sys_language_uid',
 			'searchFields' => '',
 			'title' => 'LLL:EXT:sportms/Resources/Private/Language/locallang_tca.xlf:tx_sportms_domain_model_competitionseason',
-			'transOrigDiffSourceField' => 'l10n_diffsource',
-			'transOrigPointerField' => 'l10n_parent',
 			'tstamp' => 'tstamp',
 			'versioningWS' => TRUE,
 		),
@@ -40,44 +37,6 @@
 			'1' => array('showitem' => ''),
 		),
 		'columns' => array(
-			
-	        'sys_language_uid' => [
-	            'exclude' => true,
-	            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
-	            'config' => [
-	                'type' => 'select',
-	                'renderType' => 'selectSingle',
-	                'special' => 'languages',
-	                'items' => [
-	                    [
-	                        'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
-	                        -1,
-	                        'flags-multiple'
-	                    ]
-	                ],
-	                'default' => 0,
-	            ],
-	        ],
-	        'l10n_parent' => [
-	            'displayCond' => 'FIELD:sys_language_uid:>:0',
-	            'exclude' => true,
-	            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
-	            'config' => [
-	                'type' => 'select',
-	                'renderType' => 'selectSingle',
-	                'default' => 0,
-	                'items' => [
-	                    ['', 0],
-	                ],
-	                'foreign_table' => 'tx_sportms_domain_model_competitionseason',
-	                'foreign_table_where' => 'AND {#tx_sportms_domain_model_competitionseason}.{#pid}=###CURRENT_PID### AND {#tx_sportms_domain_model_competitionseason}.{#sys_language_uid} IN (-1,0)',
-	            ],
-	        ],
-	        'l10n_diffsource' => [
-	            'config' => [
-	                'type' => 'passthrough',
-	            ],
-	        ],
 	        
 	        't3ver_label' => [
 	            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.versionLabel',
