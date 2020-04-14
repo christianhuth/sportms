b80d0eac1583d63f50b1d34d2da4e3fc8db1cdc7
Christian Knell <christian.knell@balumedien.de>
2020-04-14 14:56:24 +0200

    wip

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,32 +2,27 @@
 	defined('TYPO3_MODE') or die();
 	
 	$_EXTKEY = 'sportms';
+	$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+	$pluginNames = ['sportms', 'club', 'competition', 'game', 'person', 'team'];
 	
-	/***************
-	 * Register Plugin in the Backend Plugin List
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-		$_EXTKEY,					                                    // Extension-Key
-		'sportms',					                        // Plugin-Name
-		'Sport Management System',	                            // Plugin-Label
-		'EXT:sportms/Resources/Public/Icons/Extension.svg'     // Extension Icon in Plugin Selection
-	);
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-		$_EXTKEY,					                                     // Extension-Key
-		'club',					                             // Plugin-Name
-		'Sport Management System (Club)',                       // Plugin-Label
-		'EXT:sportms/Resources/Public/Icons/tx_sportms_domain_model_club.svg'      // Extension Icon in Plugin Selection
-	);
+	foreach($pluginNames AS $pluginName) {
+		/***************
+		 * Register Plugin in the Backend Plugin List
+		 */
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+			$_EXTKEY,					                                                                                                                            # Extension-Key
+			$pluginName,					                                                                                                                        # Plugin-Name
+			'Sport Management System (' . ucfirst($pluginName) . ')',	                                                                                    # Plugin-Label
+			# TODO: REMOVE SPORTMS-CASE
+			'EXT:sportms/Resources/Public/Icons/' . (($pluginName === 'sportms') ? 'Extension.svg' : 'tx_sportms_domain_model_' . $pluginName) . '.svg'    # Extension Icon in Plugin Selection
+		);
+		$pluginSignature = $extensionName.'_'.$pluginName;
+		$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+		$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+			$pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_sportms.xml'
+		);
+	}
 	
 	// Add Static Template File
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Sport Management System');
-	
-	$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
-	$pluginName = strtolower('sportms');
-	$pluginSignature = $extensionName.'_'.$pluginName;
-	
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-		$pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_sportms.xml'
-	);
