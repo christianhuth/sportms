96c76fd78cbfe7510bcb06a5a71344f6a53df2f0
Christian Knell <christian.knell@balumedien.de>
2020-04-15 17:00:38 +0200

    new label for GameChange

--- Configuration/TCA/UserFunc/UserFunc.php
+++ Configuration/TCA/UserFunc/UserFunc.php
@@ -96,13 +96,13 @@
 			$personOut = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord("tx_sportms_domain_model_person", $record['person_out']);
 			$newLabel = '';
 			if($personIn) {
-				$newLabel .= $personIn['lastname'] . ', ' . $personIn['firstname'];
+				$newLabel .= $personIn['lastname'];
 			}
 			if($personIn && $personOut) {
 				$newLabel .= ' <-> ';
 			}
 			if($personOut) {
-				$newLabel .= $personOut;
+				$newLabel .= $personOut['lastname'];
 			}
 			$newLabel .= ' (' . $record['minute'] . '.)';
 			$parameters['title'] = $newLabel;
