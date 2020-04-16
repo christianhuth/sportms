3f8e4cba537610aaac115547d3e2433753733435
Christian Knell <christian.knell@balumedien.de>
2020-04-17 01:55:23 +0200

    wip

--- Classes/Domain/Repository/GameLineupRepository.php
+++ Classes/Domain/Repository/GameLineupRepository.php
@@ -28,10 +28,13 @@
 		public function findPlayersWithMostGames(string $sportUids = NULL, string $sportAgeGroupUids = NULL, string $sportAgeLevelUids = NULL, string $sportPositionGroupUids = NULL, string $sportPositionUids = NULL, string $competitionTypeUids = NULL, string $competitionUids = NULL, string $clubUids = NULL, string $teamUids = NULL, string $seasonUids = NULL, int $limit = 10) {
 			$tableGameLineup = 'tx_sportms_domain_model_gamelineup';
 			$tableGameLineupAlias = 'gamelineup';
+			$tablePerson = 'tx_sportms_domain_model_person';
+			$tablePersonAlias = 'person';
 			$queryBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)->getQueryBuilderForTable($tableGameLineup);
-			$queryBuilder->SELECT($tableGameLineupAlias . '.*')
+			$queryBuilder->SELECT('*')
 							->addSelectLiteral('COUNT(' . $tableGameLineupAlias . '.' . $queryBuilder->quoteIdentifier('game') . ') AS ' . $queryBuilder->quoteIdentifier('numberOfGames'))
 							->FROM($tableGameLineup, $tableGameLineupAlias)
+							->INNERJOIN($tableGameLineupAlias, $tablePerson, $tablePersonAlias, $queryBuilder->expr()->eq($tableGameLineupAlias . '.person', $queryBuilder->quoteIdentifier($tablePersonAlias . '.uid')))
 							->GROUPBY($tableGameLineupAlias . '.person')
 							->ORDERBY('numberOfGames', \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING)
 							->setMaxResults($limit);
