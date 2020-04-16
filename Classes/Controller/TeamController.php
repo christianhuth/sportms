eb295f608bca370e745442907836e6aabd0b9928
Christian Knell <christian.knell@balumedien.de>
2020-04-16 23:24:11 +0200

    wip

--- Classes/Controller/TeamController.php
+++ Classes/Controller/TeamController.php
@@ -14,10 +14,22 @@
 		protected $clubRepository;
 		
 		/**
+		 * @var \Balumedien\Sportms\Domain\Repository\CompetitionRepository
+		 * @TYPO3\CMS\Extbase\Annotation\Inject
+		 */
+		protected $competitionRepository;
+		
+		/**
 		 * @var \Balumedien\Sportms\Domain\Repository\GameRepository
 		 * @TYPO3\CMS\Extbase\Annotation\Inject
 		 */
 		protected $gameRepository;
+		
+		/**
+		 * @var \Balumedien\Sportms\Domain\Repository\SeasonRepository
+		 * @TYPO3\CMS\Extbase\Annotation\Inject
+		 */
+		protected $seasonRepository;
 		
 		/**
 		 * @var \Balumedien\Sportms\Domain\Repository\SportRepository
@@ -65,23 +77,21 @@
 			$teams = $this->teamRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(), $this->getSportAgeLevelsFilter(), $this->getClubsFilter(), $this->getTeamsFilter());
 			$this->view->assign('teams', $teams);
 			/* FRONTEND FILTERS */
-			if($this->settings['sport']['sportsSelectbox'] || $this->settings['club']['clubsSelectbox']) {
-				if($this->settings['sport']['sportsSelectbox']) {
-					$sportsSelectbox = $this->sportRepository->findAll($this->getSportsFilter(FALSE));
-					$this->view->assign('sportsSelectbox', $sportsSelectbox);
-					if($this->settings['sport']['selected'] && $this->settings['sportAgeGroup']['sportAgeGroupsSelectbox']) {
-						$sportAgeGroupsSelectbox = $this->sportAgeGroupRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(FALSE));
-						$this->view->assign('sportAgeGroupsSelectbox', $sportAgeGroupsSelectbox);
-						if($this->settings['sportAgeGroup']['selected'] && $this->settings['sportAgeLevel']['sportAgeLevelsSelectbox']) {
-							$sportAgeLevelsSelectbox = $this->sportAgeLevelRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(), $this->getSportAgeLevelsFilter(FALSE));
-							$this->view->assign('sportAgeLevelsSelectbox', $sportAgeLevelsSelectbox);
-						}
+			if($this->settings['sport']['sportsSelectbox']) {
+				$sportsSelectbox = $this->sportRepository->findAll($this->getSportsFilter(FALSE));
+				$this->view->assign('sportsSelectbox', $sportsSelectbox);
+				if($this->settings['sport']['selected'] && $this->settings['sportAgeGroup']['sportAgeGroupsSelectbox']) {
+					$sportAgeGroupsSelectbox = $this->sportAgeGroupRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(FALSE));
+					$this->view->assign('sportAgeGroupsSelectbox', $sportAgeGroupsSelectbox);
+					if($this->settings['sportAgeGroup']['selected'] && $this->settings['sportAgeLevel']['sportAgeLevelsSelectbox']) {
+						$sportAgeLevelsSelectbox = $this->sportAgeLevelRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(), $this->getSportAgeLevelsFilter(FALSE));
+						$this->view->assign('sportAgeLevelsSelectbox', $sportAgeLevelsSelectbox);
 					}
 				}
-				if($this->settings['club']['clubsSelectbox']) {
-					$clubsSelectbox = $this->clubRepository->findAll($this->getClubsFilter(FALSE));
-					$this->view->assign('clubsSelectbox', $clubsSelectbox);
-				}
+			}
+			if($this->settings['club']['clubsSelectbox']) {
+				$clubsSelectbox = $this->clubRepository->findAll($this->getClubsFilter(FALSE));
+				$this->view->assign('clubsSelectbox', $clubsSelectbox);
 			}
 		}
 		
@@ -104,6 +114,15 @@
 			$this->view->assign('gamesWithMostSpectators', $gamesWithMostSpectators);
 			$gamesWithFewestSpectators = $this->gameRepository->findGamesWithFewestSpectatorsForTeam($teamUid, $this->settings['team']['historyRecordGames']['limit']);
 			$this->view->assign('gamesWithFewestSpectators', $gamesWithFewestSpectators);
+			/* FRONTEND FILTERS */
+			if($this->settings['competition']['competitionsSelectbox']) {
+				$competitionsSelectbox = $this->competitionRepository->findAll($this->getSportsFilter(), $this->getSportAgeGroupsFilter(), $this->getSportAgeLevelsFilter(), $this->getCompetitionTypesFilter(), $this->getCompetitionsFilter(FALSE));
+				$this->view->assign('competitionsSelectbox', $competitionsSelectbox);
+			}
+			if($this->settings['season']['seasonsSelectbox']) {
+				$seasonsSelectbox = $this->seasonRepository->findAll($this->getSeasonsFilter(FALSE));
+				$this->view->assign('seasonsSelectbox', $seasonsSelectbox);
+			}
 		}
 		
 	}
