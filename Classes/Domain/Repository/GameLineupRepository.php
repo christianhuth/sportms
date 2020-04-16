73ca977db58fcfb665bd293714b6fc39f0e17fce
Christian Knell <christian.knell@balumedien.de>
2020-04-17 01:38:18 +0200

    wip

--- Classes/Domain/Repository/GameLineupRepository.php
+++ Classes/Domain/Repository/GameLineupRepository.php
@@ -36,7 +36,6 @@
 							->ORDERBY('numberOfGames', \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING)
 							->setMaxResults($limit);
 			debug($queryBuilder->getSQL());
-			debug($this->objectType);
 			$dataMapper = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper::class);
 			return $dataMapper->map(\Balumedien\Sportms\Domain\Model\PlayerStat::class, $queryBuilder->execute()->fetchAll());
 		}
