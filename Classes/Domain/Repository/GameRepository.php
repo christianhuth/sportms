450879829a1e64ed2cd68af427275a06067955ff
Christian Knell <christian.knell@balumedien.de>
2021-01-17 13:55:33 +0100

    changed var_dump values to show complete sql statement

--- Classes/Domain/Repository/GameRepository.php
+++ Classes/Domain/Repository/GameRepository.php
@@ -49,7 +49,10 @@
 			if($constraints) {
 				$query->matching($query->logicalAnd($constraints));
 			}
-            # Folgender Schnippel debugged eine Query, die in der Variable $query gespeichert ist
+			ini_set("xdebug.var_display_max_children", '-1');
+			ini_set("xdebug.var_display_max_data", '-1');
+			ini_set("xdebug.var_display_max_depth", '-1');
+            # Folgender Schnippsel debugged eine Query, die in der Variable $query gespeichert ist
             $queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
             \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
             \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters());
