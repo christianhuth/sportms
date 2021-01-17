e46ec5a7e329b097eb7e6e0a0d2299db6cbea9cd
Christian Knell <christian.knell@balumedien.de>
2021-01-17 16:32:07 +0100

    wip

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -192,8 +192,8 @@
 			} else {
 				foreach($this->getListOfAllowedSportMsDomainModels() as $sportMsDomainModel) {
 					\TYPO3\CMS\Core\Utility\DebugUtility::debug($this->arguments[$sportMsDomainModel], 'Debug: ' . __FILE__ . ' in Line: ' . __LINE__);
-					$actionsOfDomainModel = $this->getListOfDomainModelsAndTheirActions()[$this->arguments[$sportMsDomainModel]];
 					if($this->arguments[$sportMsDomainModel]) {
+						$actionsOfDomainModel = $this->getListOfDomainModelsAndTheirActions()[$this->arguments[$sportMsDomainModel]];
 						foreach($actionsOfDomainModel as $actionOfDomainModel) {
 							if($action === $actionOfDomainModel) {
 								$this->setSportMsDomainModel($sportMsDomainModel);
