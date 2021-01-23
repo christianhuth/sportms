89ff4165a298edfc45492d89be022e68527712d0
Christian Knell <christian@knell.it>
2021-01-23 23:43:46 +0100

    wip

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -224,7 +224,7 @@
 			# add every given Domain Object as parameter for the link
             foreach($this->getListOfAllowedSportMsDomainModels() as $sportMsDomainModel) {
                 if($this->arguments[$sportMsDomainModel]) {
-                    die("TEST: " . $this->arguments[$sportMsDomainModel]);
+                    die("TEST: " . $this->arguments[$sportMsDomainModel]->getUid());
                     $parameters[lcfirst($sportMsDomainModel)] = $this->arguments[$sportMsDomainModel];
                 }
             }
