57601817fb8a9b5fec0ef6c569665b9da379224a
Christian Knell <christian@knell.it>
2021-01-23 23:37:54 +0100

    wip

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -201,7 +201,7 @@
 			} else {
 				$action = $this->arguments['action'];
 				# check if Object of Controller is given, else we can't execute the action
-                if(is_null($this->arguments[$controller])) {
+                if(!(!is_null($this->arguments[$controller])) || (($controller === "TeamSeason") && (is_null($this->arguments["Team"])))) {
                     die("No Domain Object given to display.");
                 }
 			}
