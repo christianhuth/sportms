2c71d7960bdcfd56074af660271b0f80ba00d8dd
Christian Knell <christian@knell.it>
2021-01-23 22:46:36 +0100

    check if correct Controller is given

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -195,6 +195,7 @@
 			# find out which Controller to use
 			if($this->arguments['controller']) {
 				$controller = $this->arguments['controller'];
+				die($this->getListOfControllersAndTheirActions()[$controller]);
                 (in_array($controller, $this->getListOfControllersAndTheirActions(), false)) ? die("correct Controller given") : die("Wrong Controller $controller given to create Link in sportms Extension.");
 			} else {
 			    die("No Controller given to create Link in sportms Extension.");
