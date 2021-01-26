bf353008da0238a803e361cb1edeef3d88246dda
Christian Knell <christian@knell.it>
2021-01-26 18:46:44 +0100

    wip

---
+++ Classes/ViewHelpers/WeekdayViewHelper.php
@@ -0,0 +1,24 @@
+<?php
+	
+	namespace Balumedien\Sportms\ViewHelpers;
+	
+	class ZodiacSignViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper {
+		
+		/**
+		 * @return void
+		 */
+		public function initializeArguments() {
+			parent::initializeArguments();
+			$this->registerArgument('weekyday', 'int', 'the day of the week', TRUE);
+		}
+		
+		/**
+		 * @return string
+		 */
+		public function render() {
+			$localizationUtility = new \TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+			$langKey = 'tx_sportms_general.day.' . $this->arguments['weekday'];
+			return $localizationUtility->translate($langKey, "sportms");
+		}
+		
+	}
