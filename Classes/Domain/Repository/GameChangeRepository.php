61c23c47300b59dd7d7afbb620cd6fe5846926be
Christian Knell <christian@knell.it>
2021-07-04 19:18:31 +0100

    wip

---
+++ Classes/Domain/Repository/GameChangeRepository.php
@@ -0,0 +1,16 @@
+<?php
+	
+	namespace Balumedien\Sportms\Domain\Repository;
+	
+	use Balumedien\Sportms\Domain\Model\TeamSeason;
+	use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+	
+	class GameChangeRepository extends SportMSBaseRepository {
+		
+		protected $defaultOrderings = [
+			'period' => QueryInterface::ORDER_ASCENDING,
+			'minute' => QueryInterface::ORDER_ASCENDING,
+			'minute_additional' => QueryInterface::ORDER_ASCENDING,
+		];
+	
+	}
