a006e863b0d9bdacf521af1381bdb41ff3a24d45
Christian Knell <christian@knell.it>
2021-09-02 13:58:10 +0100

    [FIX]

--- Configuration/TCA/tx_sportms_domain_model_teamseason.php
+++ Configuration/TCA/tx_sportms_domain_model_teamseason.php
@@ -13,8 +13,6 @@
             'delete' => 'deleted',
             'enablecolumns' => [
                 'disabled' => 'hidden',
-                'starttime' => 'starttime',
-                'endtime' => 'endtime',
             ],
             'hideTable' => true,
             'iconfile' => 'EXT:sportms/Resources/Public/Icons/tx_sportms_domain_model_teamseason.svg',
@@ -46,7 +44,7 @@
             ],
         ],
         'palettes' => [
-            'visibility_general' => ['showitem' => 'hidden, starttime, endtime'],
+            'visibility_general' => ['showitem' => 'hidden'],
             'visibility_detail' => ['showitem' => 'detail_link'],
         ],
         'columns' => [
@@ -73,35 +71,6 @@
                     ],
                     'renderType' => 'checkboxToggle',
                     'type' => 'check',
-                ],
-            ],
-            'starttime' => [
-                'exclude' => true,
-                'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
-                'config' => [
-                    'type' => 'input',
-                    'renderType' => 'inputDateTime',
-                    'eval' => 'datetime,int',
-                    'default' => 0,
-                    'behaviour' => [
-                        'allowLanguageSynchronization' => true,
-                    ],
-                ],
-            ],
-            'endtime' => [
-                'exclude' => true,
-                'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
-                'config' => [
-                    'type' => 'input',
-                    'renderType' => 'inputDateTime',
-                    'eval' => 'datetime,int',
-                    'default' => 0,
-                    'range' => [
-                        'upper' => mktime(0, 0, 0, 1, 1, 2038),
-                    ],
-                    'behaviour' => [
-                        'allowLanguageSynchronization' => true,
-                    ],
                 ],
             ],
             
