db2e635c5dec8853fb6fd8b5f0631e539cc6ed2e
Christian Knell <christian@knell.it>
2021-09-17 21:40:06 +0100

    wip

---
+++ Tests/Unit/Domain/Model/AddressTest.php
@@ -0,0 +1,15 @@
+<?php
+    
+    declare(strict_types=1);
+    
+    namespace Unit\Domain\Model;
+    
+    use ChristianKnell\Sportms\Domain\Model\Address;
+    use PHPUnit\Framework\TestCase;
+    
+    class AddressTest extends TestCase
+    {
+        public function test() {
+            self::assertSame(4, 2+2);
+        }
+    }
