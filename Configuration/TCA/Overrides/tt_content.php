43dd515e541ed521ed3904de7c8f6437d07894e1
Christian Knell <christian@knell.it>
2025-02-04 15:25:06 +0100

    use return value as plugin signature

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,7 +11,7 @@
     $pluginNames = ['ClubDetail', 'ClubList', 'CompetitionDetail', 'CompetitionList', 'GameDetail', 'GameList', 'PersonList', 'SeasonList', 'TeamDetail', 'TeamList'];
     foreach ($pluginNames as $pluginName) {
         // Add the Plugin to Plugin Selection DROPDOWN in the Backend
-        ExtensionUtility::registerPlugin(
+        $pluginSignature = ExtensionUtility::registerPlugin(
             $extensionName,
             $pluginName,
             $pluginName . ' Management (' . strtolower($extensionName) . ')',
@@ -19,24 +19,23 @@
                 str_replace("Detail", "", $pluginName))) . '.svg',
             'Sport Management System'
         );
-        $pluginSignature = strtolower($extensionName) . '_' . strtolower($pluginName);
         $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
         $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
         ExtensionManagementUtility::addPiFlexFormValue(
-            $pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_' . $pluginSignature . '.xml'
+            $pluginSignature,
+            'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_' . $pluginSignature . '.xml'
         );
     }
     
     // SPORTMS-PLUGIN
     $pluginName = "SportMS";
-    ExtensionUtility::registerPlugin(
+    $pluginSignature = ExtensionUtility::registerPlugin(
         $extensionName,
         $pluginName,
         'Database Statistics (' . strtolower($extensionName) . ')',
         'EXT:sportms/Resources/Public/Icons/Extension.svg',
         'Sport Management System'
     );
-    $pluginSignature = strtolower($extensionName) . '_' . strtolower($pluginName);
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
     ExtensionManagementUtility::addPiFlexFormValue(
