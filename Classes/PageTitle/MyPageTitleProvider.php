341d1ff24ee5acf6d0428470291319134baf6192
Christian Huth <christian@knell.it>
2025-07-02 11:46:36 +0200

    Update MyPageTitleProvider.php

--- Classes/PageTitle/MyPageTitleProvider.php
+++ Classes/PageTitle/MyPageTitleProvider.php
@@ -3,10 +3,8 @@
     declare(strict_types=1);
     
     namespace ChristianKnell\Sportms\PageTitle;
-    
-    use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
 
-    final class MyPageTitleProvider extends AbstractPageTitleProvider
+    class MyPageTitleProvider
     {
         
         /**
